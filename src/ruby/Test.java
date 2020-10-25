d29616f092e1b29b7e7141e941b27a5118b8985f
Ruby Khan <rubynqureshi@gmail.com>
2020-10-25 11:20:01 +0530

    test

--- src/ruby/Test.java
+++ src/ruby/Test.java
@@ -3,6 +3,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to TechnoCredits !!!");
+		System.out.println("Welcome to TechnoCredits123 !!!");
 	}
 
 }
