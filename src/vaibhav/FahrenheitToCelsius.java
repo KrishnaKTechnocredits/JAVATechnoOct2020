26953eef154e919b670e5e7b839b4adf0120eaa0
Vaibhav Jagtap <vaibhav23123@gmail.com>
2020-10-25 00:41:40 -0400

    VaibhavJ_Assignment_5

---
+++ src/vaibhav/FahrenheitToCelsius.java
@@ -0,0 +1,15 @@
+class TemperatureConvert{
+	double Fahrenheit;
+	double celsius;
+	
+	void fahrenheitToCelsius(double givenFahrenheit){
+		Fahrenheit = givenFahrenheit;
+		celsius = (Fahrenheit-32) * 5/9;
+		System.out.println("Temperature into celsius is "+celsius);
+		
+	public static void main(String[] Args)
+	{
+		TemperatureConvert temperatureConvert = new TemperatureConvert();
+		temperatureConvert.fahrenheitToCelsius(80.5);	
+	}
+}
