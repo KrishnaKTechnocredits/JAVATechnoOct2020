b8eb13b06dc019e75e9bb45c510d1330b96ff736
sureshchukka <sureshchukka@gmail.com>
2020-10-26 22:48:13 +0530

    Suresh_Assignment_5

--- src/suresh/Mall.java
+++ src/suresh/Mall.java
@@ -1,57 +1,53 @@
 package suresh;
+
 public class Mall {
-	int items;
+		int items;
+	String customername;
+	String paymentmethod;
+	boolean billrequired;
+	boolean carrybag;
+	String section;
+	
 	 void itemAddedToCart(int items) {
 		 this.items = items;
-		 System.out.println("Items Added to Cart: " + items);
-		if(items < 10) {
-		 
-			System.out.println("<--->Payment done using cash for less than 10 items<---->");
-		}
-		
-		 
-	 }
+		 }
 	 void customerName(String customername) {
-		 
-		if(items>10) {
-			 System.out.println("<--->Payment done using visa card for more than 10 items<-->");
-		System.out.println("customer Name is : " + customername);
-		}
-		 
+		 this.customername = customername;
 		 	 }
-	 void paymentDoneUsing(String paymenttype){
-		 if(items>10) {
-			 System.out.println("<--->Payment done using cash for more than 10 items<-->");
-		
-		}
+	 void paymentDoneUsing(String paymentmethod){
+		  this.paymentmethod = paymentmethod;
 	 }
-		
-	void displayCutomerInfo(String customername) {
-		if(items>10) {
-			 System.out.println("<--->Payment done using visa card for more than 10 items<-->");
-		System.out.println("customer Name is : " + customername);
-		}
-	}
-	void needBill(boolean bill ) {
-		System.out.println("Need Bill:" + bill);
-		
+	void needBill(boolean billrequired ) {
+		this.billrequired = billrequired;
 	}
 	void haveCarryBag(boolean carrybag ) {
-		
-		System.out.println("Having Carry Bag:" + carrybag);
+		this.carrybag = carrybag;		
 	}
-	void decideSection(boolean decide) {
-		System.out.println("Decide confirmation" + decide);
+	void decideSection() {
+		if(items < 10 && paymentmethod == "cash")
+			section = "Section1A";	 
+		else if (items > 10 && paymentmethod == "Visa")
+			section = "Section1B";
+		else if (items > 10 && paymentmethod == "Cash")
+			section = "Section1C";
+		else if (items < 10 && paymentmethod == "Visa")
+			section = "Section1D";
+	}
+	void displayCutomerInfo() {
+		System.out.println(customername + " has bought " + items + " number of items and payment is done using "
+				+ paymentmethod + " in " + section);
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Mall mall  = new Mall();
 		
-		mall.itemAddedToCart(12);
-		mall.customerName("Suresh");
-		mall.haveCarryBag(true);
+		mall.itemAddedToCart(9);
+		mall.customerName("Harsh");
+		mall.paymentDoneUsing("Visa");
 		mall.needBill(true);
-		
+		mall.haveCarryBag(true);
+		mall.decideSection();
+		mall.displayCutomerInfo();
 
 	}
 
