3f26f34fe158fa794257b6d7227010e3596e9797
Sadhana Pawar <pawarsadhana93@gmail.com>
2020-10-27 06:19:34 +0530

    SadhanaP_Assignment_5

---
+++ src/sadhana/Example1.java
@@ -0,0 +1,20 @@
+package sadhana;
+
+public class Example1 {
+	
+	void processData(int marks) {
+		if(marks >=90)
+			System.out.println("Excellent");
+		else if(marks >=80)
+			System.out.println("Good job");
+		else if(marks >=70)
+			System.out.println("---Average---");
+		else
+			System.out.println("Fail");
+	}
+
+	public static void main(String[] args) {
+		Example1 example1 = new Example1();
+		example1.processData(78);
+	}
+}
