fa048a9f6e99341df868e506dbccb7f23d272c40
Sadhana Pawar <pawarsadhana93@gmail.com>
2020-10-28 10:03:55 +0530

    Sadhana_Practice_PR

---
+++ src/sadhana/Car.java
@@ -0,0 +1,38 @@
+package sadhana;
+
+public class Car {
+	String companyName;
+	boolean carAutomatic;
+	double carSpeed;
+
+	void setcompanyName(String name) {
+		companyName = name;
+	}
+
+	void setIsCarAutomatic(boolean carType) {
+		carAutomatic = carType;
+	}
+
+	void setCarSpeed(double speed) {
+		carSpeed = speed;
+	}
+
+	void displaySpecification() {
+		System.out.println("------Car specifivations are------");
+		System.out.println("Company name is: " + companyName);
+		if (carAutomatic)
+			System.out.println("It is automatic car");
+		else
+			System.out.println("It is not automatic car");
+		System.out.println("Speed of car is: " + carSpeed + " kmph");
+	}
+
+	public static void main(String[] args) {
+		Car car = new Car();
+		car.setcompanyName("BMW");
+		car.setIsCarAutomatic(true);
+		car.setCarSpeed(15.50);
+		car.displaySpecification();
+	}
+
+}
