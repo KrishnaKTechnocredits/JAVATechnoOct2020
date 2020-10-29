9bfd27fc449413eccd37a38caa7ee4f8bde6c34b
apurva choudhary <apurva.sys@gmail.com>
2020-10-30 00:17:25 +0530

    Apurva_Asgmt_7

---
+++ src/apurva/NumberPrint.java
@@ -0,0 +1,43 @@
+package apurva;
+
+class NumberPrint {
+	void evenNum(int start, int end) {
+		for (int index = start; index <= end; index++) {
+			if (index % 2 == 0) {
+				System.out.println("Even numbers are: " + index);
+			}
+		}
+	}
+
+	void divNum1(int start, int end) {
+		for (int index = start; index <= end; index++) {
+			if (index % 5 == 0) {
+				System.out.println("Divisible by 5 numbers are: " + index);
+			}
+		}
+	}
+
+	void divNum2(int start, int end) {
+		for (int index = start; index <= end; index++) {
+			if (index % 5 == 0 && index % 3 == 0) {
+				System.out.println("Divisible by 5 & 3 numbers are: " + index);
+			}
+		}
+	}
+
+	void divNum3(int start, int end) {
+		for (int index = start; index <= end; index++) {
+			if (index % 7 == 0 && index % 13 == 0) {
+				System.out.println("Divisible by 7 & 13 numbers are: " + index);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		NumberPrint numberprint = new NumberPrint();
+		numberprint.evenNum(10, 15);
+		numberprint.divNum1(10, 30);
+		numberprint.divNum2(5, 18);
+		numberprint.divNum3(5, 40);
+	}
+}
