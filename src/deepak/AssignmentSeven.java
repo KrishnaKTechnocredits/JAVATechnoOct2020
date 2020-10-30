495a556a19b6b9ba1230e0b25458f7d44db286b8
Deepak Latad <dklatad@gmail.com>
2020-10-30 09:02:22 +0530

    implemented review comments

--- src/deepak/AssignmentSeven.java
+++ src/deepak/AssignmentSeven.java
@@ -3,59 +3,44 @@ package deepak;
 public class AssignmentSeven {
 	
 	void evenNumbers(int start, int end) {
-		
 		System.out.println("Printing Even numbers in given range"+" "+ start +" "+ "TO"+" "+ end);
-		
 		for(int i=start; i<=end; i++) {
-			
 			if(i%2==0) {
-				
 				System.out.println(i);
 			}
 		}
-		
 	}
 	
-	void numbersDivisibleByFive(int start, int end) {
-		
+	void numbersDivisibleByFive(int start, int end) {	
 		System.out.println("Printing numbers divisible by five only in given range"+" "+ start +" "+ "TO"+" "+ end);
-		
-		for(int i=start; i<=end; i++) {
-			
-			if(i%5==0) {
-				
+		for(int i=start; i<=end; i++) {	
+			if(i%5==0) {	
 				System.out.println(i);
 			}
 		}
-		
 	}
 	
 	void numbersDivisibleByFiveAndThree(int start, int end) {
-		
 		System.out.println("Printing numbers divisible by five and three in given range"+" "+ start +" "+ "TO"+" "+ end);
-		
 		for(int i=start; i<=end; i++) {
-			
-			if(i%5==0 && i%3==0) {
-				
+			if(i%5==0 && i%3==0) {	
 				System.out.println(i);
 			}
-		}
-		
+		}	
 	}
 	
 	void numbersDivisibleBySevernOrThirteen(int start, int end) {
-		
 		System.out.println("Printing numbers divisible by seven OR thirteen in given range"+" "+ start +" "+ "TO"+" "+ end);
-		
 		for(int i=start; i<=end; i++) {
-			
-			if(i%7==0 || i%13==0) {
-				
-				System.out.println(i);
+			if(i%7==0 || i%13==0) {	
+				if(i%7==0) {	
+					System.out.println(i +" Is divisible by"+" "+ 7);	
+				}
+				else {	
+					System.out.println(i +" Is divisible by"+" "+ 13);
+				}
 			}
-		}
-		
+		}	
 	}
 
 	public static void main(String[] args) {
