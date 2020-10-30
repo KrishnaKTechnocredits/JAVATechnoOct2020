8523ea26b049461f1581be000c753b2399da9d77
Vaibhav Jagtap <vaibhav23123@gmail.com>
2020-10-30 14:33:19 -0400

    CodeModified as per suggestion from harsh

--- src/vaibhav/Shop.java
+++ src/vaibhav/Shop.java
@@ -8,39 +8,39 @@ public class Shop {
 	int masala = 73;
 	
 	void shopping(int noOfMaggies,int noOfDosas,int noOfOilPackets,int NoOfPaniPuri,int noOfMasalas ) {		
-		if(noOfMaggies>maggie)
-			maggie = 0;
+		if(noOfMaggies > maggie)
+			System.out.println("Given quantity for maggie is not available in inventory.The available Quantity is "+maggie);
 		else 
-			maggie=maggie-noOfMaggies;
-		if(noOfDosas>dosa)
-			dosa=0;
+			maggie = maggie - noOfMaggies;
+		if(noOfDosas > dosa)
+			System.out.println("Given quantity for dosa is not available in inventory.The available Quantity is "+dosa);
 		else 
-			dosa=dosa-noOfDosas;
-		if(noOfOilPackets>oilPackets)
-			oilPackets=0;
+			dosa = dosa - noOfDosas;
+		if(noOfOilPackets > oilPackets)
+			System.out.println("Given quantity for Oil packet not available in inventory.The available Quantity is "+oilPackets);
 		else 
-			oilPackets=oilPackets-noOfOilPackets;
-		if(NoOfPaniPuri>paniPuri)
-			paniPuri=0;
+			oilPackets = oilPackets - noOfOilPackets;
+		if(NoOfPaniPuri > paniPuri)
+			System.out.println("Given quantity for panipuri not available in inventory.The available Quantity is "+paniPuri);
 		else 
-			paniPuri=paniPuri-NoOfPaniPuri;
-		if(noOfMasalas>masala)
-			masala=0;
+			paniPuri = paniPuri - NoOfPaniPuri;
+		if(noOfMasalas > masala)
+			System.out.println("Given quantity of masala is not available in inventory.The available Quantity is "+masala);
 		else 
-			masala=masala-noOfMasalas;			
+			masala = masala - noOfMasalas;			
 	}
 	
 	void displayOutOfStocksItems() {		
 		if(maggie == 0)
-			System.out.println("Maggie is out of stock");
+			System.out.println("Magiie is out of stock");
 		if(dosa == 0)
 			System.out.println("Dosa is out of stock");
 		if(oilPackets == 0)
-			System.out.println("OilPackets are out of stock");
+			System.out.println("Oil Packets are out of stock");
 		if(paniPuri == 0)
-			System.out.println("panipuri is out of stock");
+			System.out.println("PaniPuri is out of stock");
 		if(masala == 0)
-			System.out.println("masala is out of stock");
+			System.out.println("Masala is out of stock");
 	}
 	
 	void displayAvailableInStocksItems() {
@@ -58,9 +58,12 @@ public class Shop {
 
 	public static void main(String[] args) {
 		Shop shop=new Shop();
-		shop.shopping(3, 4, 5, 6, 7);
+		shop.shopping(49, 4, 5, 6, 7);
 		shop.displayOutOfStocksItems();
 		shop.displayAvailableInStocksItems();
-	}
+		shop.shopping(51, 4, 5, 6, 7);
+		shop.displayOutOfStocksItems();
+		shop.displayAvailableInStocksItems();
+		}
 
 }
