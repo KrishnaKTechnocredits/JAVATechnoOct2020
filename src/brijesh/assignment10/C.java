897e901c7da138e6168aacfd2a28911d6a9968db
BrijeshGOct2020 <brijeshgajera4321@gmail.com>
2020-11-03 10:15:53 +0530

    BrijeshG_Assignment_10

---
+++ src/brijesh/assignment10/C.java
@@ -0,0 +1,15 @@
+package brijesh.assignment10;
+
+public class C {
+
+	void m4() {
+		System.out.println("C m4");
+		C c = new C();
+		c.m5();
+	}
+	
+	void m5() {
+		System.out.println("C m5");
+		D.m6();
+	}
+}
