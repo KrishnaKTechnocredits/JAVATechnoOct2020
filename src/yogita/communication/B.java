56a38835ad025fe7685ffd614d2a26ca3875fda3
Yogita Pawale <pawaleyogitaymp@gmail.com>
2020-11-03 12:35:37 +0530

    YogitaP_assignment_10

---
+++ src/yogita/communication/B.java
@@ -0,0 +1,15 @@
+package yogita.communication;
+
+public class B {
+	
+	void m2() {
+		System.out.println("m2 of B");
+		m3();
+    }
+	
+	static void m3() {
+		System.out.println("m3 of B");
+		C c = new C();
+		c.m4();
+	}
+}
