45dd1c32a68b3aac3453d8584980d62d02487a10
HarshVegada <58640315+harshvegada@users.noreply.github.com>
2020-11-03 10:26:29 +0530

    Update ITProfessional.java

--- src/surbhi/ITProfessional.java
+++ src/surbhi/ITProfessional.java
@@ -7,39 +7,8 @@ class ITProfessional {
 	boolean isAddicatedtoCoffee;
 	boolean isAddicatedToAlcohol;
 
-  void employeeName(String empname){
-		this.empname=empname;
-	}
-	void partOnweekend(boolean doesPartyOnWeekend){
-		this.doesPartyOnWeekend=doesPartyOnWeekend;
-	}
-	void addicatedToTea(boolean isAddicatedToTea){
-		this.isAddicatedToTea=isAddicatedToTea;
-	}
-	void addicatedtoCoffee(boolean isAddicatedtoCoffee){
-		this.isAddicatedtoCoffee=isAddicatedtoCoffee;
-	}
-	void addicatedToAlcohol(boolean isAddicatedToAlcohol){
-		this.isAddicatedToAlcohol=isAddicatedToAlcohol;
-	}
-	void displaySimplication(){
-		System.out.println("Employee Name is "+ empname);
-		if(doesPartyOnWeekend){
-			System.out.println("Party is weekend " + doesPartyOnWeekend);
-		}
-		if(isAddicatedToTea){
-			System.out.println("Additced to tea " + isAddicatedToTea);
-		}
-		if(isAddicatedtoCoffee){
-			System.out.println("Additced to coffess " + isAddicatedtoCoffee);
-		}
-		if(isAddicatedToAlcohol){
-			System.out.println("Additced to Alcohol " + isAddicatedToAlcohol);
-		}
-	}
-
-	void employeeName(String empname) {
-		this.empname = empname;
+	void employeeName(String empName) {
+		this.empname = empName;
 	}
 
 	void partOnweekend(boolean doesPartyOnWeekend) {
