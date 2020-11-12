d352cea13039ca12772ed705f5903f59698cfca9
Suvela khaladkar <suvela.vela16@gmail.com>
2020-11-12 17:42:47 +0530

    suvela_assignment16

--- src/suvela/Assignment16.java
+++ src/suvela/Assignment16.java
@@ -8,17 +8,21 @@ import java.util.Scanner;
 
 public class Assignment16 {
 	void findfrequency(String word) {
-		for(int index=0;index<word.length();index++) {
+		for (int index = 0; index < word.length(); index++) {
 			char ch = word.charAt(index);
-			int counter =0;
-				for(int i=0;i<word.length();i++) {
-					if(word.charAt(i)==ch)
-						counter++;
-						
-				}
-				System.out.println("character " +ch+" is present "+counter+ " times");
+			int counter = 0;
+			for (int i = 0; i < word.length(); i++) {
+				if (word.charAt(i) == ch)
+					counter++;
+			}
+			System.out.println("character " + ch + " is present " + counter + " times");
 		}
-		
+
+	}
+	void array(String[] array) {
+		for (int index = 0; index < array.length; index++) {
+			findfrequency(array[index]);
+		}
 	}
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
@@ -26,6 +30,11 @@ public class Assignment16 {
 		String s = scanner.next();
 		Assignment16 assign = new Assignment16();
 		assign.findfrequency(s);
+		String[] array = new String[5];
+		for (int index = 0; index < array.length; index++) {
+			System.out.println("Enter string:");
+			array[index] = scanner.next();
+		}
+		assign.array(array);
 	}
-
 }
