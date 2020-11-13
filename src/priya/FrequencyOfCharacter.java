8589c6041304ddb8e43b9d7f53b391e06df959b4
Priya More <nupurmore@gmail.com>
2020-11-13 21:01:33 +0530

    PriyaM_Assigmt_16

---
+++ src/priya/FrequencyOfCharacter.java
@@ -0,0 +1,28 @@
+//Find the frequency of each character from user given word
+package priya;
+
+import java.util.Scanner;
+
+public class FrequencyOfCharacter {
+	public static void occuranceOfCharacter(String word) {
+		for (int index = 0; index < word.length(); index++) {
+			int count = 0;
+			char ch = word.charAt(index);
+			for (int i = 0; i < word.length(); i++) {
+				if (word.charAt(i) == ch)
+					count++;
+			}
+			System.out.println("Character " + ch + " is present " + count + " times");
+		}
+
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Please enter a string");
+		Scanner scanner = new Scanner(System.in);
+		String givenString = scanner.next();
+		FrequencyOfCharacter.occuranceOfCharacter(givenString);
+		scanner.close();
+	}
+}
+
