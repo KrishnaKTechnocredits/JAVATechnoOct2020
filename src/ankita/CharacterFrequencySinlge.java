af3c0ed7e7a96995f051016a0e2b381c52dea573
Ankita Dahiwelkar <ankitadahiwelkar52@gmail.com>
2020-11-13 14:41:15 +0530

    AnkitaM_Asgmnt_17

---
+++ src/ankita/CharacterFrequencySinlge.java
@@ -0,0 +1,35 @@
+/*
+ Same as Assignment 16 but each frequency should be printed single time.
+input : aakanksha
+output : a -> 4
+              k -> 2
+              n -> 1
+              s -> 1
+              h -> 1
+ */
+package ankita;
+
+import java.util.Scanner;
+
+public class CharacterFrequencySinlge {
+	void findCharFreqSingleTime(String strng) {
+		for(int i=0;i<strng.length();i++) {
+			int index=strng.indexOf(strng.charAt(i));
+			if(i==index) {
+				int cnt=0;
+				for(int j=0;j<strng.length();j++) {
+					if(strng.charAt(i)==strng.charAt(j))
+						cnt++;
+				}
+				System.out.println(strng.charAt(i)+"->"+cnt);
+			}
+		}
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter String to find charachetr frequency single time:");
+		String str = sc.next();
+		CharacterFrequencySinlge characterFrequencySinlge=new CharacterFrequencySinlge();
+		characterFrequencySinlge.findCharFreqSingleTime(str);
+	}
+}
