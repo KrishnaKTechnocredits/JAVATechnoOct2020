79c79b94a81598d5f4257f118bffca42d511c6a1
Monika Sethi <sethi.mona90@gmail.com>
2020-11-17 08:55:40 +0530

    MonikaS_Assignment_20

---
+++ src/monika/SwapStringWithoutThirdVariable.java
@@ -0,0 +1,29 @@
+package monika;
+/*Assignment-20 :
+Program : 4
+swap 2 String without using 3rd variable
+Hint : use SubString method of String class
+
+*/
+
+public class SwapStringWithoutThirdVariable {
+
+	public static void swap(String a,String b) {
+
+		a=a+b;//jagtapvaibhav
+		b=a.substring(0, a.length() -b.length());
+		a=a.substring(b.length());
+		System.out.println("After Swaping");
+		System.out.println("String A : "+a+" String B : "+b);
+	}
+
+	public static void main(String[] args) {
+		String a = "sethi";
+		String b ="monika";
+		System.out.println("Before Swaping");
+		System.out.println("String A : "+a+" String B : "+b);
+		SwapStringWithoutThirdVariable.swap(a,b);
+	}
+
+
+}
