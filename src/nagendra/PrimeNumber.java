6379d4601c6caa572eed081c4e26ec5e24c66dde
Nagendra Tiwari <tiwarinagendra11@gmail.com>
2020-11-19 01:13:24 +0530

    NagendraT_Assignment_22

---
+++ src/nagendra/PrimeNumber.java
@@ -0,0 +1,29 @@
+package nagendra;
+
+
+public class PrimeNumber {
+	
+	void verifyPrimeNumber(int number) {
+		boolean flag = true;
+		for(int i=2;i<=Math.sqrt(number);i++) {
+			if(number%i == 0) {
+				flag = false;
+				break;
+			}
+		}
+		 if(flag)
+			System.out.print(number + ","); 	
+	}
+	
+	void getPrimeNumberFromRange(int from,int to){
+		System.out.println("Prime Numbers from "+from+" to "+to+" are");
+		for(int index=from;index<to;index++){
+			verifyPrimeNumber(index);
+		}
+	}
+	
+	public static void main(String[] args) {
+		PrimeNumber primeNumber = new PrimeNumber();
+		primeNumber.getPrimeNumberFromRange(2,100);
+	}
+}
