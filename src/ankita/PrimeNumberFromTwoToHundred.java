800f257b86773fadbf9c78ea59b4bc4dd8d58472
Ankita Dahiwelkar <ankitadahiwelkar52@gmail.com>
2020-11-18 12:24:28 +0530

    AnkitaM_Asgmnt_22

--- src/ankita/PrimeNumberFromTwoToHundred.java
+++ src/ankita/PrimeNumberFromTwoToHundred.java
@@ -11,6 +11,8 @@ public class PrimeNumberFromTwoToHundred {
 		int primeArray[] = new int[num2-num1]; 
 		int count=1;
 		for(int i=num1;i<=num2;i++) {
+			if(i==0 || i==1)
+				continue;
 			boolean flag=true;
 			for(int j=2;j<=i/2;j++) {
 				if(i%j==0) {
@@ -24,7 +26,7 @@ public class PrimeNumberFromTwoToHundred {
 		return primeArray;
 	}
 	public static void main(String[] args) {
-		int[] arr =  new PrimeNumberFromTwoToHundred().getPrimeNumberFromTwoToHundred(2,100);
+		int[] arr =  new PrimeNumberFromTwoToHundred().getPrimeNumberFromTwoToHundred(0,100);
 		for(int i=0;i<arr.length;i++) {
 			if(arr[i]!=0)
 				System.out.print(arr[i]+ ", ");
