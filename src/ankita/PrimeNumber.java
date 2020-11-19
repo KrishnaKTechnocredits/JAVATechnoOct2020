87160b3537c9c8d22e484f5cb33bfb137f617428
Ankita Dahiwelkar <ankitadahiwelkar52@gmail.com>
2020-11-19 09:12:11 +0530

    AnkitaM_Asgmnt_22_otherProgram

---
+++ src/ankita/PrimeNumber.java
@@ -0,0 +1,32 @@
+//optional program from sunday lecture -Check if Given number is prime or not
+package ankita;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+	
+	void checkPrime(int number) {
+		boolean flag = true;
+		if(number!=0 && number!=1) {
+		for(int i=2;i<=number/2;i++) {
+			if(number%i==0) {
+				flag = false;
+				break;
+			}
+		}
+		if(!flag)
+			System.out.println(number + " Not is prime number");
+		else 
+			System.out.println(number + " is Prime Number");
+		}
+		else
+			System.out.println(number + " is not a prime");
+		}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number to check if prime or not");
+		int number = sc.nextInt();
+		PrimeNumber primeNumber = new PrimeNumber();
+		primeNumber.checkPrime(number);
+	}
+}
