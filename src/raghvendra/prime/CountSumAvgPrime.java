8fae2fa95b542cc0e53366783b6114dfba36399f
RaghvendramaniTOct2020 <raghvendramani.tiwari@gmail.com>
2020-11-19 23:06:25 -0500

    RaghvendraT_assignment_23

---
+++ src/raghvendra/prime/CountSumAvgPrime.java
@@ -0,0 +1,29 @@
+package raghvendra.prime;
+//Find count,sum,Average of prime numbers from 2 to 200
+public class CountSumAvgPrime {
+	void printCountSumAvgPrime() {
+		int count=0;;
+		int sum=0;
+		boolean flag=false;
+		for(int index=2;index<=200;index++) {
+			flag=false;
+			for(int inner=2;inner<=Math.sqrt(index);inner++) {
+				if(index%inner==0) {
+					flag=true;
+					break;
+				}
+			}
+			if(!flag) {
+			count++;
+			sum=sum+index;
+			}
+		}
+		System.out.println("Count of Primr numbers:"+count);
+		System.out.println("Sum of Primr numbers:"+sum);
+		System.out.println("Average of Primr numbers:"+(sum/count));
+	}
+	public static void main(String[] args) {
+		CountSumAvgPrime countSumAvgPrime=new CountSumAvgPrime();
+		countSumAvgPrime.printCountSumAvgPrime();
+	}
+}
