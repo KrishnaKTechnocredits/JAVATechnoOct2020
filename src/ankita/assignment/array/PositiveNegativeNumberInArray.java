4b2725ad6f5c28b6c5df1a13f3425b677bb17cc8
Ankita Dahiwelkar <ankitadahiwelkar52@gmail.com>
2020-11-20 13:03:46 +0530

    AnkitaM_Asgmnt_23_packageRearranged

---
+++ src/ankita/assignment/array/PositiveNegativeNumberInArray.java
@@ -0,0 +1,37 @@
+/*
+ 	Create a class where user will provide int array. *[ideal Expected Time : 15 mins, Max 
+    Time : 25 mins]*
+    A) User can provide any positive and negative number
+    B) You need to find out how many positive, negative numbers and zeros are there in array 
+ */
+package ankita.assignment.array;
+
+import java.util.Scanner;
+
+public class PositiveNegativeNumberInArray {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("How many numbers yoy want to store:");
+		int number = sc.nextInt();
+		int[] numberArray = new int[number];
+		for (int i = 0; i < number; i++) {
+			System.out.println("Enter number");
+			numberArray[i] = sc.nextInt();
+		}
+		int positiveCnt = 0, negativeCnt = 0, zeroCnt = 0;
+		for (int i = 0; i < numberArray.length; i++) {
+			if (numberArray[i] > 0)
+				positiveCnt++;
+			if (numberArray[i] < 0)
+				negativeCnt++;
+			if (numberArray[i] == 0)
+				zeroCnt++;
+		}
+		if (positiveCnt > 0)
+			System.out.println(positiveCnt + " positive Number present");
+		if (negativeCnt > 0)
+			System.out.println(negativeCnt + " negative Number present");
+		if (zeroCnt > 0)
+			System.out.println(zeroCnt + " Zero present");
+	}
+}
---
+++ src/ankita/assignment/prime/PrimeFromTwoToTwoHundred.java
@@ -0,0 +1,50 @@
+// Find count,sum & avg of prime number from 2-200.
+package ankita.assignment.prime;
+
+public class PrimeFromTwoToTwoHundred {
+	int[] getPrimeFromTwoToTwoHundred() {
+		int cnt = 0;
+		int arr[] = new int[198];
+		for (int i = 2; i <= 200; i++) {
+			boolean flag = false;
+			for (int j = 2; j <= i / 2; j++) {
+				if (i % j == 0) {
+					flag = true;
+					break;
+				}
+			}
+			if (!flag) {
+				arr[cnt++] = i;
+			}
+		}
+		System.out.println("Count of prime number from 2 to 200 : " + cnt);
+		return arr;
+	}
+
+	int getSumOfPrimeFromTwoToTwoHundred(int[] arr) {
+		int sum = 0;
+		for (int i = 0; i < arr.length; i++) {
+			sum += arr[i];
+		}
+		System.out.println("Sum of prime number from 2 to 200 :" + sum);
+		return sum;
+	}
+
+	void getAvgOfPrimeFromTwoToTwoHundred(int[] arr) {
+		int sum = 0, cnt = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] != 0) {
+				sum += arr[i];
+				cnt++;
+			}
+		}
+		System.out.println("Avg of prime number from 2 to 200 :" + sum / cnt);
+	}
+
+	public static void main(String[] args) {
+		PrimeFromTwoToTwoHundred twoToTwoHundred = new PrimeFromTwoToTwoHundred();
+		int arr[] = twoToTwoHundred.getPrimeFromTwoToTwoHundred();
+		int sum = twoToTwoHundred.getSumOfPrimeFromTwoToTwoHundred(arr);
+		twoToTwoHundred.getAvgOfPrimeFromTwoToTwoHundred(arr);
+	}
+}
