6d7ee71e94757f2eaeead6fe8318c9208c854d16
Pooja Pekhale <pekhalepooja9999@gmail.com>
2020-11-20 12:49:23 +0530

    PoojaP_Assignment_22

--- src/poojap/PrimeNumberr.java
+++ src/poojap/PrimeNumberr.java
@@ -2,26 +2,26 @@ package poojap;
 
 public class PrimeNumberr {
 
-	void findPrimeNumber(int start, int end) {
+	void findPrime(int start, int end) {
 
 		System.out.println("Prime Numbers from " + start + " to " + end);
 
 		for (int i = start; i <= end; i++) {
 			int num = i;
-			boolean flag = true;
+			boolean flag =true;
 			for (int j = 2; j <= num / 2; j++) {
 				if (num % j == 0)
 					flag = false;
 			}
-			if (!flag) {
+			if (flag) {
 				System.out.print(i + " ");
 			}
 		}
-	}
+		}
 
 	public static void main(String[] args) {
 		PrimeNumberr prime = new PrimeNumberr();
-		prime.findPrimeNumber(2, 100);
+		prime.findPrime(2, 100);
 
 	}
 }
