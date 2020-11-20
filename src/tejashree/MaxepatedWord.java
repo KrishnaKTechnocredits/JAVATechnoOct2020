ef78c81d425866f3c56f6d381850427c77ca0772
Tejashree Thorat <tejashree.thorat1@gmail.com>
2020-11-20 20:13:32 +0530

    TejashreeT_Assignment18

--- src/tejashree/MaxepatedWord.java
+++ src/tejashree/MaxepatedWord.java
@@ -8,23 +8,23 @@ public class MaxepatedWord {
 	void WordFrequency(String inputString) {
 		int max = 0;
 		String maxOccuringWord = null;
-		String[] inputSplitedArray = inputString.split(" ");
-		for (int index = 0; index < inputSplitedArray.length; index++) {
-			String matchWord = inputSplitedArray[index];
+		String[] inputSplitArray = inputString.split(" ");
+		for (int index = 0; index < inputSplitArray.length; index++) {
+			String matchWord = inputSplitArray[index];
 			int freqCount = 0;
 			if (inputString.contains(matchWord)) {
-				for (int occurenceIndex = 0; occurenceIndex < inputSplitedArray.length; occurenceIndex++) {
-					if (matchWord.equals(inputSplitedArray[occurenceIndex]))
+				for (int occurenceIndex = 0; occurenceIndex < inputSplitArray.length; occurenceIndex++) {
+					if (matchWord.equals(inputSplitArray[occurenceIndex]))
 						freqCount++;
 				}
 			}
 			if (freqCount > max) {
 				max = freqCount;
-				maxOccuringWord = inputSplitedArray[index];
+				maxOccuringWord = inputSplitArray[index];
 			}
 			inputString = inputString.replace(maxOccuringWord, " ");
 		}
-		System.out.println("Maximum occuring word is '" + maxOccuringWord + "and no of times it is repeated is " + max + " times.");
+		System.out.println("Maximum occuring word is '" + maxOccuringWord +"'"+"and no of times it is repeated is " + max + " times.");
 	}
 
 	public static void main(String[] args) {
