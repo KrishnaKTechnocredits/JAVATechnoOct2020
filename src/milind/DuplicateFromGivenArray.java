0af7088e66f658dd3c3576fc049fdc39310e6f42
Milind Chavhan <ms.chavhan81@gmail.com>
2020-11-21 12:48:23 +0530

    MilindC_Exam_2_20Min

--- src/milind/DuplicateFromGivenArray.java
+++ src/milind/DuplicateFromGivenArray.java
@@ -1,3 +1,12 @@
+/* Exam3: find duplicate from given array.
+input : int[] arr = {10,12,55,32,17,12,32};
+output : 12
+         32
+
+String[] names = {"Techno", "Credits","Techno","Hi","Hello","Hi"};
+output : Techno
+         Hi */
+
 package milind;
 
 public class DuplicateFromGivenArray {
