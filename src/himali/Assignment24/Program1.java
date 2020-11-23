b03fed6d3d58582f1987447c2514450ce2e78127
Himali Ingle <himaliingle20@gmail.com>
2020-11-23 17:50:58 +0530

    Himali _assignment 24 submitted

---
+++ src/himali/Assignment24/Program1.java
@@ -0,0 +1,26 @@
+package himali.Assignment24;
+
+public class Program1 {
+	int getNumberSum(String input) {
+		int sum=0;
+		for(int i=0;i<input.length();i++) {
+			char ch=input.charAt(i);
+			
+				if(Character.isDigit(ch)) {
+					sum=sum+Character.getNumericValue(ch);
+				}
+		}
+		return sum;
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Program1 program1=new Program1();
+		int sumOfDights=program1.getNumberSum("te2ch9kj");
+		System.out.println("Sum of each digit is " +sumOfDights);
+		
+
+	}
+
+}
