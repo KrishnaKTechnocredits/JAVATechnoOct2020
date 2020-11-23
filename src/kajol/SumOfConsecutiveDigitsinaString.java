f0b6040fa4d1673f43b2934717823825277f8a9f
Kajol Chhabada <kajalcchhabada@gmail.com>
2020-11-24 01:20:08 +0530

    KajolC_Assignment_24

---
+++ src/kajol/SumOfConsecutiveDigitsinaString.java
@@ -0,0 +1,31 @@
+package kajol;
+
+import java.util.Scanner;
+
+public class SumOfConsecutiveDigitsinaString {
+	
+	int sum=0;
+	String temp="0";
+	void SumofConsecutiveDigits(String inputString) {
+		for(int index=0;index<inputString.length();index++) {
+			if(Character.isDigit(inputString.charAt(index)))
+				temp+=inputString.charAt(index);
+			else {
+				sum+=Integer.parseInt(temp);
+			    temp="0";
+			}
+		}
+		sum+=Integer.parseInt(temp);
+		System.out.println("Given String is: "+inputString);
+		System.out.println("Sum of Digts: "+sum);	
+	}
+	
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("Please Enter a AlphaNumeric String");
+		new SumOfConsecutiveDigitsinaString().SumofConsecutiveDigits(scanner.next());
+		scanner.close();
+	}
+}
+
+
