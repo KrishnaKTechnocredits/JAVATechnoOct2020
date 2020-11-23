73d5868779fbbd939d05bccbdebab3ca3a7a48a5
Deepak Latad <dklatad@gmail.com>
2020-11-23 08:05:38 +0530

    DeepakL_Asgmt_17

---
+++ src/deepak/FrequencyOFEachCharInString.java
@@ -0,0 +1,29 @@
+package deepak;
+
+import java.util.Scanner;
+
+public class FrequencyOFEachCharInString {
+
+	public static void occuranceOfEachChar(String word) {
+		for (int i = 0; i < word.length(); i++) {
+			int count = 0;
+			char ch = word.charAt(i);
+			int index = word.indexOf(word.charAt(i));
+			if(i==index) {
+			for (int j = 0; j < word.length(); j++) {
+				if (word.charAt(i) == word.charAt(j))
+					count++;
+			}
+			System.out.println(ch + " -> " + count);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Please enter a word");
+		Scanner scanner = new Scanner(System.in);
+		String input = scanner.next();
+		FrequencyOFEachCharInString.occuranceOfEachChar(input);
+		scanner.close();
+	}
+}
