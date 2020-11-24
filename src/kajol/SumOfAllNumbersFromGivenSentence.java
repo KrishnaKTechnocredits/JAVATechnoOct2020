bb08d9285fb65089676288cfc5a44daf9f89c5bd
Kajol Chhabada <kajalcchhabada@gmail.com>
2020-11-24 22:16:01 +0530

    KajolC_Assignment_25

--- src/kajol/SumOfAllNumbersFromGivenSentence.java
+++ src/kajol/SumOfAllNumbersFromGivenSentence.java
@@ -1,3 +1,12 @@
+/*Assignment - 25 : 22nd Nov'2020 
+
+Program 1: sum of all numbers from give string.  
+
+String msg = I have 15 years and 2 months of experience.
+
+hint : Integer.parseInt(String input) will help to convert string to int.
+you may need spilt, charAt method as well. */
+
 package kajol;
 
 import java.util.Scanner;
