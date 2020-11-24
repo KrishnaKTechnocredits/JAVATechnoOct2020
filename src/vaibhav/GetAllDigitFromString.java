449e480a41223bb45ba07ccde9e0696f8a3aa473
Vaibhav Jagtap <vaibhav23123@gmail.com>
2020-11-23 19:44:13 -0500

    VaibhavJ_Assignment_24

---
+++ src/vaibhav/GetAllDigitFromString.java
@@ -0,0 +1,31 @@
+package vaibhav;
+
+public class GetAllDigitFromString {
+	
+	void getAllDigit(String str) {
+		int sum = 0;
+		String temp = "";
+		boolean flag = false;
+		for (int index = 0 ; index < str.length();index++) {
+			char ch=str.charAt(index);
+			if(Character.isDigit(ch)) {
+				flag = true;
+				temp = temp+Character.getNumericValue(ch);				
+			}
+			if (Character.isLetter(ch) && flag == true  || index == str.length()-1 )
+			{
+				sum = sum + Integer.parseInt(temp);
+				flag = false;
+				temp = "";
+			}
+		}		
+		System.out.println("Addition of number from given string is "+sum);
+ 	}
+
+	public static void main(String[] args) {		
+		String given="te123ch9kj8";
+		GetAllDigitFromString getLLDigitFromString = new GetAllDigitFromString();
+		getLLDigitFromString.getAllDigit(given);
+	}
+
+}
