f9a809a605afafa21df12783d83f22fe4b4d8b11
Tejashree Thorat <tejashree.thorat1@gmail.com>
2020-11-24 19:58:17 +0530

    Tejashree_Assignment26

---
+++ src/tejashree/Assignment26.java
@@ -0,0 +1,19 @@
+package tejashree;
+/*convert all capital letters to small and small to capitial.
+String msg = "tE1GfrEsT";
+output : Te1gFReSt*/
+public class Assignment26 {
+	public static void main(String[] args) {
+		String msg = "tE1GfrEsT";
+		char ch = msg.charAt(0);
+		for (int index = 0; index < msg.length(); index++) {
+			ch = msg.charAt(index);
+			if (Character.isUpperCase(ch)) {
+				ch = Character.toLowerCase(ch);
+			} else if (Character.isLowerCase(ch)) {
+				ch = Character.toUpperCase(ch);
+			}
+			System.out.print(ch);
+		}
+	}
+}
