74c33fffd965c76ef77220493d0d1f3944c6c3a5
Prem <prem.khandegar@gmail>
2020-11-25 00:31:46 +0530

    Assignement 22

---
+++ src/prem/PrimeNumber.java
@@ -0,0 +1,28 @@
+package prem;
+
+//Assignment-22 : 18th Nov'2020 
+//Print all prime numbers between 2 to 100.
+//A number that can be divided exactly only by itself and 1, for example 7, 17 and 41
+
+public class PrimeNumber {
+	
+	void findPrimeNumber(int number) {
+		boolean flag = true;
+		for(int i=2;i<=Math.sqrt(number);i++) {
+			if(number%i==0) {
+				System.out.println("Numbers is not Prime = " + number);
+				flag = false;
+			}
+		}
+		if(flag)
+			System.out.println(number + "is prime");
+				
+		}
+	
+	public static void main(String[] args) {
+		PrimeNumber primeNumber = new PrimeNumber();
+		for (int j=0; j<=200; j++) {
+		primeNumber.findPrimeNumber(j);
+		}
+	}
+}
