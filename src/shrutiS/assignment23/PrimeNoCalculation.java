a177bcea474d1219d83cfee378937b02a2cc743f
Shruti Sattigeri <shruti.sattigeri040591@gamil.com>
2020-11-25 21:22:25 +0530

    ShrutiS_Assignment_23

--- src/shrutiS/assignment23/PrimeNoCalculation.java
+++ src/shrutiS/assignment23/PrimeNoCalculation.java
@@ -9,10 +9,8 @@ package shrutiS.assignment23;
 public class PrimeNoCalculation {
 	float sum;
 	int finalCount;
-	int count;
 
 	int findCountOfPrimeNo() {
-
 		for (int index = 2; index <= 100; index++) {
 			int count = 0;
 			for (int innerIndex = 1; innerIndex <= index / 2; innerIndex++) {
@@ -36,21 +34,20 @@ public class PrimeNoCalculation {
 					count++;
 			}
 			if (count == 1)
-				sum = sum + index; 
+				sum = sum + index;
 		}
 		System.out.println("Total sum of all prime numbers between 2-100 : " + sum);
 		return sum;
 	}
 
 	void findAverageOfPrimeNo(float sum, int totalCount) {
-		System.out.println("Average of all prime numbers between 2-100 : "+(sum/totalCount));
+		System.out.println("Average of all prime numbers between 2-100 : " + (sum / totalCount));
 	}
 
 	public static void main(String[] args) {
 		PrimeNoCalculation primeNo = new PrimeNoCalculation();
-		int totalCount= primeNo.findCountOfPrimeNo();
-		float sum= primeNo.findSumOfPrimeNo();
-		primeNo.findAverageOfPrimeNo(sum,totalCount);
-
+		int totalCount = primeNo.findCountOfPrimeNo();
+		float sum = primeNo.findSumOfPrimeNo();
+		primeNo.findAverageOfPrimeNo(sum, totalCount);
 	}
 }
