7d7d5883a239cf6644d2b64c3d3d507b41bd1514
Suvela khaladkar <suvela.vela16@gmail.com>
2020-11-26 20:21:34 +0530

    Suvela_asgn_28

--- src/suvela/assignment28/Assign28.java
+++ src/suvela/assignment28/Assign28.java
@@ -3,29 +3,30 @@
 output : maximum sum triplet 20,18,22 --> 60*/
 package suvela.assignment28;
 
+import java.util.Arrays;
+
 public class Assign28 {
 	void maxSumOfTriplet(int[] array) {
+		System.out.println("Given Array " +Arrays.toString(array));
 		int max = 0;
-		int temp2 = 0;
-		int temp1 = 0;
-		int temp = 0;
-		for (int i = 0; i < array.length; i++) {
-			for (int j = i + 1; j < array.length; j++) {
-				for (int k = j + 1; k < array.length; k++) {
-					if (max < array[i] + array[j] + array[k]) {
-						max = array[i] + array[j] + array[k];
-						temp2 = array[k];
-						temp1 = array[j];
-						temp = array[i];
-					}
-				}
+		int sum = 0;
+		int index = 0;
+		int cnt = 0;
+		for (int i = 0; i < array.length && cnt < (array.length / 3); i = i + 3) {
+			sum = array[i] + array[i + 1] + array[i + 2];
+			if (max < sum) {
+				max = sum;
+				index = i;
 			}
+			cnt++;
 		}
-		System.out.print("Maximum sum of triplet :" + temp + "," + temp1 + "," + temp2 + ":>" + max);
+		System.out.print("Maximum sum of triplet :" + array[index] + "," + array[index + 1] + "," + array[index + 2]
+				+ ":>" + max);
+
 	}
 
 	public static void main(String[] args) {
-		int arr[] = { 10, 12, 8, 20, 18, 22, 15, 13, 17 };
+		int arr[] = { 10, 12, 8, 20, 18, 22, 15, 13, 17,200,400,900};
 		Assign28 assign = new Assign28();
 		assign.maxSumOfTriplet(arr);
 	}
