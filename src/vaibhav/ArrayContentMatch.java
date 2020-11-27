5413a05fa18ffe0f1a7fb2e396a46a89011f7290
Vaibhav Jagtap <vaibhav23123@gmail.com>
2020-11-26 22:59:03 -0500

    VaibhavJ_Exam_4

--- src/vaibhav/ArrayContentMatch.java
+++ src/vaibhav/ArrayContentMatch.java
@@ -5,7 +5,7 @@ package vaibhav;
 input: 
 
 int array1 [] = [1, 2, 5, 5, 8, 9, 7, 10]
-int array2 [] = [1, 0, 6, 15, 6, 4, 7, 0]
+int array2 [] = [1, 0, 6, 15, 6, 7, 4, 0]
 
 output: 1 7*/
 
@@ -14,16 +14,22 @@ public class ArrayContentMatch {
 	public static void arrayMatch(int[] array1, int[] array2) {
 		System.out.println("Common Elements are");
 		boolean flag = false;
-		for (int index = 0; index < array1.length;index++ ) {
-			if(array1[index] == array2[index])
-				System.out.print(" "+array1[index]);
+		for (int index = 0; index < array1.length;index++ ) {			
+			int temp = array1[index];				
+			for (int innerIndex = 0; innerIndex < array2.length;innerIndex++ ) {
+				if (temp == array2[innerIndex]) {
+					System.out.println(""+temp);
+				}			
+			}
+				
 		}	
+		
 	}
 
 	public static void main(String[] args) {
 		
-		int array1 [] = {1, 2, 5, 5, 8, 9, 7, 10};
-		int array2 [] = {1, 0, 6, 15, 6, 4, 7, 0};
+		int array1 [] = {1, 2, 5, 15, 8, 9, 7, 10};
+		int array2 [] = {1, 0, 6, 15, 6, 7, 4, 0};
 		ArrayContentMatch.arrayMatch(array1,array2);	
 
 	}
