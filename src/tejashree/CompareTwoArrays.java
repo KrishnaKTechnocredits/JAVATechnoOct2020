8e47a17d6a59eba9446d1cfc8dd0edb4ff9447b4
Tejashree Thorat <tejashree.thorat1@gmail.com>
2020-11-27 08:20:44 +0530

    Coding_Exam4_Tejashree

---
+++ src/tejashree/CompareTwoArrays.java
@@ -0,0 +1,31 @@
+/*Program 1: WAP to Find Common Element Between Two Arrays
+
+input: 
+
+int array1 [] = [1, 2, 5, 5, 8, 9, 7, 10]
+int array2 [] = [1, 0, 6, 15, 6, 4, 7, 0]
+
+output: 1 7
+*/
+
+package tejashree;
+
+public class CompareTwoArrays {
+
+	public void compareArrays(int []a1, int []a2) {
+		for(int index=0;index<a1.length;index++) {
+			for(int innerIndex=0;innerIndex<a2.length;innerIndex++) {
+				if(a1[index]==a2[innerIndex]) {
+					System.out.print(a1[index]+" ");
+				}
+			}
+		}
+		}
+	
+	public static void main(String[] args) {
+		int array1 [] = {1, 2, 5, 5, 8, 9, 7, 10};
+		int array2 [] = {1, 0, 6, 15, 6, 4, 7, 0};
+		System.out.println("Identical elements in two arrays are:");
+		new CompareTwoArrays().compareArrays(array1, array2);
+	}
+}
