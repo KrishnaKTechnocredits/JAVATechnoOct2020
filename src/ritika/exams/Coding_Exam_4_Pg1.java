866edb92d9fc344fbc39025aed18c233b876bf26
Ritika Gandhi <ritz2693@gmail.com>
2020-11-27 17:39:07 +0530

    Made required changes in Array program as requested

--- src/ritika/exams/Coding_Exam_4_Pg1.java
+++ src/ritika/exams/Coding_Exam_4_Pg1.java
@@ -11,21 +11,19 @@ output: 1 7
 public class Coding_Exam_4_Pg1 {
 
 	static void findCommonElementsBetweenArrays(int[] array1, int[] array2) {
-		if (array1.length == array2.length || array1.length < array2.length) {
-			System.out.println("Common elements:");
-			for (int index = 0; index < array1.length; index++) {
-				if (array1[index] == array2[index])
-					System.out.print(array1[index] + " ");
+		String commonElements = "";
+		System.out.println("Common elements:");
+		for (int index = 0; index < array1.length; index++) {
+			for (int ii=0; ii<array2.length; ii++) {
+				if (array1[index] == array2[ii]) {
+					commonElements +=array1[index] + " ";
+					break;
+					}	
+				}
 			}
-		} else if (array1.length > array2.length) {
-			System.out.println("Common elements:");
-			for (int index = 0; index < array2.length; index++) {
-				if (array1[index] == array2[index])
-					System.out.print(array1[index] + " ");
-			}
-		}
-	}
-
+			System.out.print(commonElements);
+		} 
+	
 	public static void main(String[] args) {
 		int array1[] = { 1, 2, 5, 5, 8, 9, 7, 10 };
 		int array2[] = { 1, 0, 6, 15, 6, 4, 7, 0 };
