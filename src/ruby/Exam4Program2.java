2fde27fa42cfa0697039be407eea39c1c7ab0226
Ruby Khan <rubynqureshi@gmail.com>
2020-11-27 09:18:01 +0530

    RubyK_CodingExam_4

--- src/ruby/Exam4Program2.java
+++ src/ruby/Exam4Program2.java
@@ -16,34 +16,32 @@ output 2: both string are identical*/
 public class Exam4Program2 {
 	void CompareString(String s1, String s2) {
 		boolean flag = false;
-		boolean sameWord = false;
+		int count = 0;
 		for (int i = 0; i < s1.length(); i++) {
-			sameWord = false;
 			if (s1.length() == s2.length()) {
 				flag = true;
-				if (s1.charAt(i) == s2.charAt(i)) {
-					sameWord = true;
+				if (s1.charAt(i) != s2.charAt(i)) {
+					count++;
 				}
 			}
 		}
 		if (!flag) {
 			System.out.println("Strings length is not same");
-		} else if (sameWord) {
-			System.out.println("Both Strings are identicals");
-		} else {
+		} else if (count > 0) {
 			System.out.println("Both Strings are not identicals");
+		} else {
+			System.out.println("Both Strings are identicals");
 		}
 	}
 
 	public static void main(String[] args) {
 		Exam4Program2 exam = new Exam4Program2();
-		String s1 = "technoCredits";
-		String s2 = "teChnoCreditS";
+		String s1 = "teChnoCreditS";
+		String s2 = "technoCredits";
 		exam.CompareString(s1, s2);
 		String s3 = "pune";
 		String s4 = "pune";
 		exam.CompareString(s3, s4);
 
 	}
-
 }
