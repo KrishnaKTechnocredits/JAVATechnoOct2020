4c41a5e47b63e57230d1cfa9785694af8a340df1
Prasad Chitale <prasad.chitale@gmail.com>
2020-11-27 08:29:52 +0530

    Prasad_C_Coding_exam_4_29mins

---
+++ src/prasad/Findiftwostrareindentical.java
@@ -0,0 +1,34 @@
+package prasad;
+
+public class Findiftwostrareindentical {
+	
+	boolean scanStr(String input1 , String input2) {
+		boolean flag = false;
+		int count=0;
+		for(int index=0 ; index<input1.length(); index++) {
+			if(input1.length()!=input2.length() || input1.charAt(index)!=input2.charAt(index))
+				break;
+			
+			else if(input1.charAt(index)== input2.charAt(index)) {
+					count++;
+			}
+			
+			if(count == input1.length())
+				flag=true;
+		}
+		
+		return flag;
+	}
+	
+	public static void main(String []arg) {
+		Findiftwostrareindentical findiftwostrareindentical = new Findiftwostrareindentical();
+		String input1 = "technoCredits";
+		String input2 = "technocredits";
+		boolean output = findiftwostrareindentical.scanStr(input1, input2);
+		if(output)
+			System.out.println("Two strings are indentical");
+		else 
+			System.out.println("Two strings are NOT indentical");
+			
+	}
+}
