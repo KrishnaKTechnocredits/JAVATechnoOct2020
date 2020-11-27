fbe2855ad018f37fe69bb0583d912c6f977ecae1
Ritika Gandhi <ritz2693@gmail.com>
2020-11-27 08:39:10 +0530

    RitikaG Coding_Exam_4_35mins

---
+++ src/ritika/exams/Coding_Exam_4_Pg2.java
@@ -0,0 +1,36 @@
+package ritika.exams;
+
+import java.util.Arrays;
+
+/*Coding Exam 4
+
+Program 2: WAP to find two String are idential or not
+
+input 1:
+String s1 = "technoCredits";
+String s2 = "teChnoCreditS";
+
+output 1: both string are not idential
+
+input 2:
+String s1 = "pune";
+String s2 = "pune";
+
+output 2: both string are idential
+*/
+public class Coding_Exam_4_Pg2 {
+	
+	static void findIfTwoStringsIdentical (String s1, String s2) {
+		System.out.println("Input--> s1: " +s1+ " and s2:" +s2);
+		if (s1.compareTo(s2) == 0)
+			System.out.println("Output --> Both strings are idential");
+		else
+			System.out.println("Output --> Both strings are not identical");
+	}
+	
+	public static void main(String[] args) {
+		Coding_Exam_4_Pg2.findIfTwoStringsIdentical("technoCredits", "teChnoCreditS");
+		System.out.println("===================================================");
+		Coding_Exam_4_Pg2.findIfTwoStringsIdentical("pune", "pune");
+	}
+}
