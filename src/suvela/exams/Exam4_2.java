9825a847d4e5f4bebc559c07463512f4e43cadd2
Suvela khaladkar <suvela.vela16@gmail.com>
2020-11-27 08:19:36 +0530

    Suvela_coding_exam4

---
+++ src/suvela/exams/Exam4_2.java
@@ -0,0 +1,49 @@
+
+/*Program 2: WAP to find two String are identical or not
+
+input 1: 
+String s1 = "technoCredits";
+String s2 = "teChnoCreditS";
+
+output 1: both string are not identical
+
+input 2:
+String s1 = "pune";
+String s2 = "pune";
+
+output 2: both string are identical*/
+package suvela.exams;
+
+public class Exam4_2 {
+	void isStringIdentical(String s, String s2) {
+		System.out.println("given strings are:" +s+","+s2);
+		boolean flag=false;
+		for(int i=0;i<s.length();i++) {
+			char ch= s.charAt(i);
+			for(int j=0;j<s2.length();j++) {
+				char ch1=s2.charAt(j);
+				if(s.length()!=s2.length())
+					System.out.println("Words length do not match");
+				else if(Character.isLowerCase(ch)==Character.isLowerCase(ch1) && Character.isUpperCase(ch)==Character.isUpperCase(ch1)) 
+						flag=true;
+				else
+					flag=false;
+			}
+			
+		}
+		if(flag)
+			System.out.println("both strings are identical");
+		else
+			System.out.println("both strings are not identical");
+		
+	}
+	public static void main(String[] args) {
+		String s1 = "technoCredits";
+		String s2 = "teChnoCreditS";
+		new Exam4_2().isStringIdentical(s1, s2);
+		String s3= "pune";
+		String s4="pune";
+		new Exam4_2().isStringIdentical(s3, s4);
+	}
+
+}
