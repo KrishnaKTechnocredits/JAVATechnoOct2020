271a14ee3f3ff1bdda0a7b4ed1df51e1e80599ea
Prem <prem.khandegar@gmail>
2020-11-28 18:56:25 +0530

    Prem_Assignment27

--- src/prem/assignment27/WordDigitSum.java
+++ src/prem/assignment27/WordDigitSum.java
@@ -19,14 +19,14 @@ public class WordDigitSum {
 				}
 			}
 			if (sum > 10)
-				System.out.println("The word where digit sum is > 10 : " + word);
+				System.out.println("The word digit sum is > 10 : " + word);
 		}
 	}
 
 	public static void main(String[] arg) {
 		WordDigitSum wordDigitSum = new WordDigitSum();
 		String[] arr = { "Ma1s3gj3Pw", "r4f3sjk", "te3ch2no9", "sfjk4p4plpk", "sfjk8p4plpk" };
-		System.out.println(" The given String is : " + Arrays.toString(arr));
+		System.out.println(" The String is : " + Arrays.toString(arr));
 		wordDigitSum.printWordDigitSum(arr);
 	}
 }
