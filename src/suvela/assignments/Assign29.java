5a4a280483e5c7843aa90b8bc8a8f7ec76ec8c0d
Suvela khaladkar <suvela.vela16@gmail.com>
2020-11-28 08:09:27 +0530

    Suvela_assignment29

--- src/suvela/assignments/Assign29.java
+++ src/suvela/assignments/Assign29.java
@@ -9,28 +9,27 @@ public class Assign29 {
 		System.out.println("Given array:" + Arrays.toString(array));
 		int max = 0;
 		int secondMax = 0;
-		if(array[0]>array[1]) {
-			max=array[0];
-		secondMax=array[1];
-		}else {
-			max=array[1];
-			secondMax=array[2];
+		if (array[0] > array[1]) {
+			max = array[0];
+			secondMax = array[1];
+		} else {
+			max = array[1];
+			secondMax = array[2];
 		}
 		for (int i = 2; i < array.length; i++) {
 			int num = array[i];
-			if(num>max) {
-				secondMax=max;
-				max=num;
-				num=0;
-			}else if(secondMax<num && num<max)
-				secondMax=num;
-			
+			if (num > max) {
+				secondMax = max;
+				max = num;
+			} else if (secondMax < num && num < max)
+				secondMax = num;
+
 		}
 		System.out.println("second maximum number is:" + secondMax);
 	}
 
 	public static void main(String[] args) {
-		int[] array = { 400,200,150,250,350,700 };
+		int[] array = { 400, 200, 850, 250, 350, 900 };
 		new Assign29().findSecondMax(array);
 
 	}
