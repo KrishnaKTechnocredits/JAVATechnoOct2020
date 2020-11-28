611b064a93f870a950fbdca0ddbcb0727071cd88
Tejashree Thorat <tejashree.thorat1@gmail.com>
2020-11-28 11:40:15 +0530

    Tejashree_Assignment29

---
+++ src/tejashree/Assignment29.java
@@ -0,0 +1,24 @@
+package tejashree;
+
+public class Assignment29 {
+	void findSecondMaxInArray(int[] arr) {
+		int max=arr[0];
+		int secondMax=0;		
+		for(int index=2;index<arr.length;index++) {
+			if(arr[index]>max) {
+				secondMax=max;
+				max=arr[index];
+			}
+			else if(arr[index]>secondMax) {
+				secondMax=arr[index];
+			}
+		}
+		System.out.println("Max element is:"+max);
+		System.out.println("Second max element is:"+secondMax);
+	}
+	public static void main(String[] args) {
+		int arr[]= {10,-1,60,30,2,90,80,110};
+		new Assignment29().findSecondMaxInArray(arr);
+	}
+
+}
