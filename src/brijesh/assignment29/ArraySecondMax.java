726b667b4ceab8f524844257ab662e0edad062bd
BrijeshGOct2020 <brijeshgajera4321@gmail.com>
2020-11-28 10:07:44 +0530

    BrijeshG_Assignment_29_Updated

--- src/brijesh/assignment29/ArraySecondMax.java
+++ src/brijesh/assignment29/ArraySecondMax.java
@@ -7,29 +7,36 @@ Find second maximum from array without sorting the array.
 */
 
 public class ArraySecondMax {
-	
-	public int findMax(int[] input) {
-		int max=input[0];
-		for(int index=1; index<input.length; index++) {
-			if(input[index]>max)
-				max=input[index];
-			}
-		return max;
-	}
-	
+
 	public void findSecondMax(int[] input) {
-		int max=findMax(input);
-		int secondMax=input[0];
-		for(int index=1; index<input.length; index++) {
-			if(input[index]>secondMax && input[index]<max)
-				secondMax=input[index];
+		System.out.println("Input Array: " + Arrays.toString(input));
+		if (input.length >= 2) {
+			int max = input[0];
+			int secondMax = input[1];
+			for (int index = 2; index < input.length; index++) {
+				if (input[index] > max) {
+					max = input[index];
+				} else if (input[index] > secondMax && input[index] != max)
+					secondMax = input[index];
+			}
+			System.out.println("Second Max: " + secondMax);
+			System.out.println("**************************************************");
+		} else {
+			System.out.println("Array length has to be 2 or more. Please provide valid array");
 		}
-		System.out.println("Second Max: "+secondMax);
 	}
 
 	public static void main(String[] args) {
-		int[] input = {10,77,20,6,40,55,33,99};
-		System.out.println("Input Array: "+Arrays.toString(input));
-		new ArraySecondMax().findSecondMax(input);
+		int[] input1 = { 10, 77, 20, 6, 40, 55, 33, 99 };
+		int[] input2 = { -20, -5, 10, -67, -39, -10 };
+		int[] input3 = { -10, 0, 20 };
+		int[] input4 = { 9, 50 };
+		int[] input5 = { 9 };
+		ArraySecondMax arraySecondMax = new ArraySecondMax();
+		arraySecondMax.findSecondMax(input1);
+		arraySecondMax.findSecondMax(input2);
+		arraySecondMax.findSecondMax(input3);
+		arraySecondMax.findSecondMax(input4);
+		arraySecondMax.findSecondMax(input5);
 	}
 }
