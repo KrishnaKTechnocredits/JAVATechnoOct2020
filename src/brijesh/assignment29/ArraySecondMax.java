b93d75ec76026b1de17a2d1625cd1dfef2b3e2f4
BrijeshGOct2020 <brijeshgajera4321@gmail.com>
2020-11-28 10:17:54 +0530

    BrijeshG_Assignment_29_Updated

--- src/brijesh/assignment29/ArraySecondMax.java
+++ src/brijesh/assignment29/ArraySecondMax.java
@@ -19,6 +19,13 @@ public class ArraySecondMax {
 				} else if (input[index] > secondMax && input[index] != max)
 					secondMax = input[index];
 			}
+			if(input.length==2) {
+				if(secondMax>max) {
+					int temp = max;
+					max= secondMax;
+					secondMax=temp;
+				}
+			}
 			System.out.println("Second Max: " + secondMax);
 			System.out.println("**************************************************");
 		} else {
