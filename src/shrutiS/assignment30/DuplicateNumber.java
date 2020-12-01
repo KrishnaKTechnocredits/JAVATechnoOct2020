7a9918b5ab325b070703323fe6253d4e6edaf3a9
Shruti Sattigeri <shruti.sattigeri040591@gamil.com>
2020-12-01 21:40:33 +0530

    ShrutiS_Assignment_30

---
+++ src/shrutiS/assignment30/DuplicateNumber.java
@@ -0,0 +1,28 @@
+/*Program 2:  Find duplicate number on Integer array in Java? [I know you have done this before, practice it one more time please]
+Hint: There is exactly one number is repeated in the array. You need to write a program to
+find that duplicate number. For example, if an array with length 6 contains numbers{0, 3,
+1, 2, 3}, then duplicated number is 3. 
+*/
+package shrutiS.assignment30;
+
+public class DuplicateNumber {
+	
+	void findDuplicateNumber(int[] input) {
+		for(int index =0; index<input.length; index++) {
+			for(int innerIndex = index+1; innerIndex<input.length; innerIndex++) {
+				if(input[index] == input[innerIndex])
+					System.out.println("Duplicate number is "+input[index]);
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		DuplicateNumber duplicateNumber = new DuplicateNumber();
+		int[] input= {0,3,1,2,3};
+		duplicateNumber.findDuplicateNumber(input);
+	}
+	
+}
+	
+
+
