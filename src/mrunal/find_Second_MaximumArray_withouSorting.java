66bedbef2027e9aa27db8772420ef78692a094e5
Mrunal Yadav <yadavmrunal94@gmail.com>
2020-12-01 12:39:57 +0530

    Mrunal_Assignment_29

---
+++ src/mrunal/find_Second_MaximumArray_withouSorting.java
@@ -0,0 +1,38 @@
+package mrunal;
+/* /*Assignment - 29 : 26th Nov'2020 
+Find second maximum from array without sorting the array.
+*/
+
+import java.util.Arrays;
+
+public class find_Second_MaximumArray_withouSorting {
+
+	void findSecondMaxNumber(int[] arr) {
+		int maxFirst = 0;
+		int maxSecond = 0;
+
+		if (arr[0] > arr[1]) {
+			maxFirst = arr[0];
+			maxSecond = arr[1];
+		} else {
+			maxFirst = arr[1];
+			maxSecond = arr[0];
+		}
+		for (int i = 2; i < arr.length; i++) {
+			if (arr[i] > maxFirst) {
+				maxSecond = maxFirst;
+				maxFirst = arr[i];
+
+			} else if (arr[i] > maxSecond) {
+				maxSecond = arr[i];
+			}
+		}
+		System.out.println("Input :"+ Arrays.toString(arr));
+		System.out.println("Second Maximum number: " + maxSecond);
+	}
+
+	public static void main(String[] args) {
+		int arr[] = { 5, 12, 10, 54, 89, 81, 20, 93 };
+		new find_Second_MaximumArray_withouSorting().findSecondMaxNumber(arr);
+	}
+}
