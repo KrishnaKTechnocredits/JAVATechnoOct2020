f4cf7abaa792a1f595b4a9b7631be1d5b17545a3
Priyanka kamthe <priya8542@gmail.com>
2020-12-02 10:18:57 +0530

    Priyanka_Assignment_30

--- src/priyanka/Assignment30/FindMissing.java
+++ src/priyanka/Assignment30/FindMissing.java
@@ -8,28 +8,26 @@ Hint: You have given an integer array which contains numbers from 1 to 10 but ex
 */
 
 public class FindMissing {
-	void findMissingNumber(int[] arr1, int[] arr2) {
-
-		for (int i = 0; i < arr1.length; i++) {
-			boolean flag = true;
-			for (int j = 0; j < arr2.length; j++) {
-				if (arr1[i] == arr2[j]) {
-					flag = false;
-					break;
-				}
-			}
-			if (flag)
-				System.out.print("Missing Number:" + arr1[i] + " ");
-
+	void findMissingNumber(int[] arr) {
+		
+		//System.out.println("Given Array: "+Arrays.toString(input));
+		int sum=0;
+		int n= arr.length+1;
+		int total =(n*(n+1))/2;
+		for(int index=0; index<arr.length; index++) {
+			sum+=arr[index];
 		}
+		//System.out.println("Total no is " + total);
+		System.out.println("**********************************************");
+		System.out.println("Missing Number is: "+(total-sum));
+		
 	}
-
+	
 	public static void main(String[] args) {
 		FindMissing missingNumber = new FindMissing();
-		int[] arr2 = { 12, 11, 1, 5, 6, 7, 2, 4, 3, 9, 10 };
-		int[] arr1 = { 12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+		int[] arr1 = { 1, 5, 6, 7, 2, 4, 3, 9, 10, 12, 8 };
 		System.out.println("Arrays is:" + Arrays.toString(arr1));
-		missingNumber.findMissingNumber(arr1, arr2);
+		missingNumber.findMissingNumber(arr1);
 
 	}
 }
