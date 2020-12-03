d78ffb0722f11d01018627294ee00259fbf8d12c
unknown <nandinisingh89@gmail.com>
2020-12-04 00:10:33 +0530

    NandiniS_Assignment_32

---
+++ src/nandini/ConsecutiveNumbers.java
@@ -0,0 +1,18 @@
+package nandini;
+
+public class ConsecutiveNumbers {
+	
+	void printConsecutiveNumbers(int[] num) {
+		for(int i=0; i<num.length-2; i++) {
+			
+	    	if((num[i+1] == num[i]+1) && (num[i+2] == num[i]+2))
+				System.out.println(num[i] +","+ num[i+1]+ ","+ num[i+2]+"->" + "Hi");
+		}
+	}
+	
+	public static void main(String args[]) {
+		int[] inputArr = {1,2,3,5,8,9,11,12,13,14,15,18,19,20};
+		new ConsecutiveNumbers().printConsecutiveNumbers(inputArr);		
+	}
+
+}
