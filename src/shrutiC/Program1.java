9e628e1d296f17d78efde9f1ffaf069c8f317fd6
Shruti Chavan <shrutichavan26@gmail.com>
2020-12-04 01:38:21 +0530

    ShrutiC_Assignment_32

---
+++ src/shrutiC/Program1.java
@@ -0,0 +1,46 @@
+package shrutiC;
+
+import java.util.Scanner;
+
+/*
+ * Program 1:  return true if UpperCase characters count are more than lowercase else return false.
+	input : TechnoCrediTS
+	output : false
+*/
+
+public class Program1 {
+
+	boolean isUppercaseCntHigher(String string) {
+
+		int upperCaseCnt = 0, lowerCaseCnt = 0;
+
+		for (int i = 0; i < string.length(); i++) {
+			char ch = string.charAt(i);
+			if (Character.isUpperCase(ch)) {
+				upperCaseCnt++;
+
+			} else if (Character.isLowerCase(ch)) {
+				lowerCaseCnt++;
+			}
+		}
+		
+		if(upperCaseCnt>lowerCaseCnt)
+			return true;	
+		else
+			return false;
+	}
+	
+	public static void main(String[] args) {
+		Program1 object = new Program1();
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter a string :");
+		String input = sc.next();
+		if(object.isUppercaseCntHigher(input))
+			System.out.println("Output : "+true+" (Upper case char count is more)");
+		else
+			System.out.println("Output : "+false+" (Lower case char count is more)");
+
+	}
+
+}
---
+++ src/shrutiC/Program3.java
@@ -0,0 +1,44 @@
+package shrutiC;
+
+public class Program3 {
+
+	// Method which take one character and return its ascii value.
+	int getAsciiValue(char ch) {
+		int asciiValue = 0;
+		return asciiValue = (int) ch;
+	}
+
+	// Method which take one ascii and return character
+	char getChar(int asciiValue) {
+		char ch = ' ';
+		return ch = (char) asciiValue;
+	}
+
+	// write a method to print ascii values for uppercase letters [A-Z].
+	// A -> 65, B - 66....
+	void printASCIIValueOfUpperCaseChar() {
+		for (int i = 65; i <= 90; i++) {
+			System.out.println(getChar(i) + " -> " + i);
+		}
+	}
+
+	// write a method to print ascii values for lowercase letter [a-z].
+	// a -> 97, b -> 98
+	void printASCIIValueOfLowerCaseChar() {
+		for (int i = 97; i <= 122; i++) {
+			System.out.println(getChar(i) + " -> " + i);
+		}
+	}
+
+	public static void main(String[] args) {
+
+		Program3 object = new Program3();
+
+		System.out.println("ASCII value of A - " + object.getAsciiValue('A')+"\n");
+		System.out.println("Char having ASCII value 66 - " + object.getChar(66)+"\n");
+
+		object.printASCIIValueOfLowerCaseChar();
+		object.printASCIIValueOfUpperCaseChar();
+	}
+
+}
