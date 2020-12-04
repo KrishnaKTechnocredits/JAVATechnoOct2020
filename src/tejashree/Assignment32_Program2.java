5ceb6bf2fd4a6d899f1acb3c4bd4d4b247a49863
Tejashree Thorat <tejashree.thorat1@gmail.com>
2020-12-04 08:31:35 +0530

    Tejashree_Assignment32

--- src/tejashree/Assignment32_Program2.java
+++ src/tejashree/Assignment32_Program2.java
@@ -11,7 +11,7 @@ output :1,2,3 -> Hi
 public class Assignment32_Program2 {
 
 	public void threeConsecutiveNumbers(int[] arr) {
-		for (int i = 2; i < arr.length-2; i++)
+		for (int i = 0; i < arr.length-2; i++)
 			if ((arr[i + 1] == arr[i] + 1) && (arr[i + 2] == arr[i] + 2)) {
 				System.out.println(arr[i] + "," + arr[i + 1] + "," + arr[i + 2] + "-> Hi");
 			}
