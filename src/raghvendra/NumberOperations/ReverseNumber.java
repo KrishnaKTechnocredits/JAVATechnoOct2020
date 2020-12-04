a9584fc310d222d6ee00a134fed99443bad0f692
RaghvendramaniTOct2020 <raghvendramani.tiwari@gmail.com>
2020-12-03 20:23:13 -0500

    RaghvendraT_Assignment_33

--- src/raghvendra/NumberOperations/ReverseNumber.java
+++ src/raghvendra/NumberOperations/ReverseNumber.java
@@ -5,12 +5,13 @@ output : 432*/
 public class ReverseNumber {
 	void reverseNumber(int number){
 		int reverseNumber=0;
+		int givenNumber=number;
 		while(number>0) {
 			int num=number%10;
 			reverseNumber=(reverseNumber*10)+num;
 			number=number/10;
 		}
-		System.out.println(reverseNumber);
+		System.out.println("Reverse number of number "+givenNumber+" is "+reverseNumber);
 	}
 	public static void main(String[] args) {
 		ReverseNumber reverseNumber=new ReverseNumber();
