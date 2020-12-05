c4a1c589abac7f18877994180adb89649009a0ae
Nitin Jain <nitin1002jain.com>
2020-12-06 01:13:49 +0530

    NitinJ_Assignment_30

--- src/nitin/FindDuplicateNoInArray.java
+++ src/nitin/FindDuplicateNoInArray.java
@@ -8,17 +8,18 @@ public class FindDuplicateNoInArray {
 	public void getDuplicateNoInArray(int[] arr) {
 		System.out.println("Input Array --> "+Arrays.toString(arr));
 		int num=0;
-		for(int index=0;index<arr.length;index++) {
-			for(int inner=0;inner<arr.length;inner++) {
-				if(arr[index]==arr[inner])
+		for(int index=0;index<arr.length-1;index++) {
+			for(int inner=index+1;inner<arr.length;inner++) {
+				if(arr[index]==arr[inner]) {
 					num=arr[inner];
+				}	
 			}
 		}
 		System.out.println("Duplicate number in the Input array --> "+num);
 	}
 	public static void main(String[] args) {
 		FindDuplicateNoInArray findDuplicateNoInArray = new FindDuplicateNoInArray();
-		int[] input= {0,3,1,2,3}; 
+		int[] input= {0,3,1,2,3,5,6,1,3,5,5,5}; 
 		findDuplicateNoInArray.getDuplicateNoInArray(input);
 	}
 }
