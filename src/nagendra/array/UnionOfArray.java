1ebcda6569356b6728a028d2568045073460ff87
Nagendra Tiwari <tiwarinagendra11@gmail.com>
2020-12-09 22:33:57 +0530

    NagendraT_Assignment_36

--- src/nagendra/array/UnionOfArray.java
+++ src/nagendra/array/UnionOfArray.java
@@ -11,61 +11,23 @@ Method Signature : double[] getUnionOfArray(double[] arr1, double[] arr2)
 public class UnionOfArray {
 
 	double[] getUnionOfArray(double[] arr1, double[] arr2) {
-		double[] userInput1 = UnionOfArray.removeDuplicateFromArray(arr1);
-		double[] userInput2 = UnionOfArray.removeDuplicateFromArray(arr2);
-		double[] output = new double[userInput1.length + userInput2.length];
-		int count = 0;
-		int num = 0;
-		boolean flag = false;
-		for (int i = 0; i < userInput2.length; i++) {
-			output[i] = userInput1[i];
-			flag = true;
-			for (int j = 0; j < userInput1.length; j++) {
-				if (userInput2[i] == userInput1[j]) {
-					num++;
-					flag = false;
-					break;
-				} else {
-					flag = true;
-					continue;
-				}
-			}
-			if (flag) {
-				output[userInput1.length + count] = userInput2[i];
-				count++;
-			}
-		}
-		double[] systemOutput = new double[output.length - num];
-		for (int k = 0; k < systemOutput.length; k++) {
-			systemOutput[k] = output[k];
-		}
-		return systemOutput;
-	}
 
-	static double[] removeDuplicateFromArray(double[] input) {
-		int count = 0;
-		int num = 0;
-		boolean flag = true;
+		double[] input = new double[arr1.length + arr2.length];
+		System.arraycopy(arr1, 0, input, 0, arr1.length);
+		System.arraycopy(arr2, 0, input, arr1.length, arr2.length);
 		double[] output = new double[input.length];
-		for (int i = 0; i < input.length; i++) {
-			flag = true;
-			for (int j = 0; j < input.length; j++) {
-				if (input[i] == input[j] && i > j) {
-					flag = false;
-					num++;
-					break;
+		int i = -000;
+		for (int j = 0; j < input.length; j++) {
+			for (int k = 0; k < input.length; k++) {
+				if (input[j] == input[k] && j != k) {
+					input[k] = i;
 				}
 			}
-			if (flag)
-				output[count] = input[i];
-			count++;
+			if (input[j] != i) {
+				output[j] = input[j];
+			}
 		}
-		double[] sysOutput = new double[output.length - num];
-
-		for (int k = 0; k < sysOutput.length; k++) {
-			sysOutput[k] = output[k];
-		}
-		return sysOutput;
+		return output;
 	}
 
 	public static void main(String[] args) {
@@ -75,7 +37,8 @@ public class UnionOfArray {
 		double[] arr2 = { 17.20, 13.30, 10.45, 18.35, 84.33, 13.30 };
 		double[] output = array.getUnionOfArray(arr1, arr2);
 		for (int i = 0; i < output.length; i++) {
-			System.out.print(output[i] + " ,");
+          if(output[i]!=0.0)
+        	  System.out.print(output[i]+" ,");
 		}
 	}
 }
