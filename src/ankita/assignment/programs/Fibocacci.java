30310b87c8ddfa31a4943ffbdedf9ef57c914b2d
Ankita Dahiwelkar <ankitadahiwelkar52@gmail.com>
2020-12-10 12:17:41 +0530

    Ankita_Asgnmnt_37

---
+++ src/ankita/assignment/programs/Fibocacci.java
@@ -0,0 +1,25 @@
+/*Assignment - 37 : 10th Dec'2020
+Print Fibonacci series for first 10 numbers.
+output : 0, 1, 1, 2, 3, 5, 8, 13, 21, 34*/
+
+package ankita.assignment.programs;
+
+public class Fibocacci {
+	void getFibonacci(int n) {
+		System.out.println("First " + n + " Fibonacci Numbers : ");
+		int n1 = 0, n2 = 1, cnt = 0, n3 = 0;
+		System.out.print(n1 + ",");
+		System.out.print(n2 + ",");
+		while (cnt < (n - 2)) {
+			n3 = n1 + n2;
+			System.out.print(n3 + ",");
+			n1 = n2;
+			n2 = n3;
+			cnt++;
+		}
+	}
+
+	public static void main(String[] args) {
+		new Fibocacci().getFibonacci(10);
+	}
+}
