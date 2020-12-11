64704dfc8e23555b70189e351b702e815c4b2a29
Vaibhav Jagtap <vaibhav23123@gmail.com>
2020-12-11 16:37:23 -0500

    VaibhavJ_Assignment_37 && Chnages in Assignment_36

--- src/vaibhav/assignment36/UnionArray.java
+++ src/vaibhav/assignment36/UnionArray.java
@@ -1,5 +1,7 @@
 package vaibhav.assignment36;
 
+import java.util.Arrays;
+
 /*Assignment : 36 [6th Dec'2020] 
 
 Program 1: Find union of two arrays.
@@ -12,48 +14,36 @@ Method Signature : double[] getUnionOfArray(double[] arr1, double[] arr2)*/
 public class UnionArray {
 	
 	public static double[] getUnionOfArray(double[] arr1, double[] arr2) {
-		double [] union = new double[arr1.length + arr2.length];
-		int k = 0;		
-		for (int i = 0 ; i < arr1.length; i++) { //Adding numbers from array 1 to union and checking for duplicate
-			boolean flag = true; 
-			double num = arr1[i];
-			for(int j = 0 ; j< union.length;j++) {
-				if (num == union[j]) {
-					flag = false;
-					break;
-				}
-			}if(flag == true) {
-				union[k]=arr1[i];
-				k++;
+		double []array3= {10.45, 14.0, 18.35, 88.88, 54.10, 18.35,17.20, 13.30, 10.45, 18.35, 84.33, 13.30};
+		double [] union = new double[array3.length];
+		int count = 0;
+		for (int i = 0 ; i < array3.length;i++) {
+			double temp= array3[i] ;
+			boolean flag=checkDuplicate(temp,union);
+			if(!flag) {
+				union[count++]=temp;
 			}			
-		}
-		for (int i = 0 ; i < arr2.length; i++) { //Adding numbers from array 2 to union array and checking for duplicate
-			boolean flag = true; 
-			double num = arr2[i];
-			for(int j = 0 ; j< union.length;j++) {
-				if (num == union[j]) {
-					flag = false;
-					break;
-				}
-			}if(flag == true) {
-				union[k]=arr2[i];
-				k++;
-			}			
-		}
+		}		
+		return union;		
+	}	
 
-		for(int j = 0 ; j< union.length;j++) {
-			System.out.print(","+union[j]);
+	private static boolean checkDuplicate(double temp, double[] union) {
+		boolean flag = false;
+		for (int j = 0 ;j< union.length;j++) {
+			if (temp == union[j]) {
+				flag = true;
+			}
 		}
-		
-		return union;	
+		return flag;		
 	}
-
-	
 
 	public static void main(String[] args) {
 		double [] arr1 = {10.45, 14.0, 18.35, 88.88, 54.10, 18.35};
         double [] arr2 = {17.20, 13.30, 10.45, 18.35, 84.33, 13.30};
         double[] union=UnionArray.getUnionOfArray(arr1,arr2);
+        for (int i = 0; i< union.length;i++) {
+        	System.out.print(","+union[i]);
+        }
 
 	}
 
