ad2efd43ed396b625d99d4b3f07766003943269c
Tejashree Thorat <tejashree.thorat1@gmail.com>
2020-12-14 19:07:44 +0530

    Tejashree_Assignment36

---
+++ src/tejashree/Assignment36_Program2.java
@@ -0,0 +1,39 @@
+package tejashree;
+
+import java.util.Arrays;
+
+/*
+Program 2: Find intersection(common elements) of two arrays.
+
+Input: arr1 -> {10.45, 14.0, 18.35, 88.88, 54.10, 18.35,18.35}
+      arr2 -> {17.20, 13.30, 10.45, 18.35, 84.33, 13.30}
+Output: 10.45, 18.35
+*/
+public class Assignment36_Program2 {
+		void getIntersectionOfArray(double[] array1, double[] array2) {
+			System.out.println("Array1:" + Arrays.toString(array1));
+			System.out.println("Array2:" + Arrays.toString(array2));
+			System.out.println("\nIntersection Of Array is:");
+
+			for (int i = 0; i < array1.length; i++) {
+				boolean flag = false;
+				for (int j = 0; j < array2.length; j++) {
+					if (array1[i] == array2[j] && i < j) {
+						flag = true;
+						break;
+					}
+				}
+				if (flag)
+					System.out.print(array1[i] + ",");
+			}
+		}
+
+		public static void main(String[] args) {
+			double[] input1 = { 10.45, 14.0, 18.35, 88.88, 54.10, 18.35 };
+			double[] input2 = { 17.20, 13.30, 10.45, 18.35, 84.33, 13.30 };
+			new Assignment36_Program2().getIntersectionOfArray(input1,input2);
+
+		}
+
+	}
+
