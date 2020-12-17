53444070f1007bde2307be422e80deed205f7616
Ankita Dahiwelkar <ankitadahiwelkar52@gmail.com>
2020-12-17 22:41:36 +0530

    Ankita_Asgnmnt_39

---
+++ src/ankita/assignment/constructor/Addition.java
@@ -0,0 +1,28 @@
+/*Program - 4:
+Create a class Addition and write add method for:
+a) add two integers
+b) add two decimal
+c) add two String
+*/
+package ankita.assignment.constructor;
+
+public class Addition {
+	public void add(int num1, int num2) {
+		System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+	}
+
+	public void add(float num1, float num2) {
+		System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+	}
+
+	public void add(String num1, String num2) {
+		System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+	}
+
+	public static void main(String[] args) {
+		Addition addition = new Addition();
+		addition.add(5, 6);
+		addition.add(4.5f, 6.0f);
+		addition.add("techno", "Credit");
+	}
+}
---
+++ src/ankita/assignment/constructor/Square.java
@@ -0,0 +1,21 @@
+/*Program : 2
+Create a Square class with field side, one parameterized constructor which accept side value .
+Write a method to find the area of a square. Write a TestSquareClass to verify Square class
+functionality.
+NOTE : square method should not take any parameter, constructor should set side value in
+instance variable and square method should use that instance variable.
+(area=side*side)
+*/
+package ankita.assignment.constructor;
+
+public class Square {
+	int side;
+
+	public Square(int side) {
+		this.side = side;
+	}
+
+	int getAreaOfSquare() {
+		return side * side;
+	}
+}
