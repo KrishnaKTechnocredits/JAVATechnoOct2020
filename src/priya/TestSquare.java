786c80f31ab4d7941f0b3e79bcde93bef58e1b4b
Priya More <nupurmore@gmail.com>
2020-12-18 19:49:30 +0530

    PriyaM_Assignment_39

---
+++ src/priya/TestSquare.java
@@ -0,0 +1,10 @@
+//Program 2  continue
+package priya;
+
+public class TestSquare {
+
+	 public static void main(String[] args) {
+    	 Square square = new Square(12);
+    	 square.areaOfSquare();
+     }
+}
