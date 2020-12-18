d1e9ea90854c8b0d6de11e6f9f2a4416046d563b
Suvela khaladkar <suvela.vela16@gmail.com>
2020-12-18 20:53:17 +0530

    Suvela_assignment40

---
+++ src/suvela/assignment40/prog2/MeetingRoom.java
@@ -0,0 +1,40 @@
+/*Program - 2:  
+Create a class MeetingRoom having method bookMeetingRoom which should provide
+below facility.
+meeting room : Nalanda, Takshashila, AgraFort, PratapGadh
+a. “Nalanda” meeting room will be booked with default bookMeetingRoom() method
+for 1 hr.
+b. Write another method which helps to book Other then “Nalanda” meeting room by
+giving name of meeting room and hrs as parameter.
+c. displayInfo method should print booked meeting room info [i.e. meeting room,
+booked for how many hrs]
+*/
+package suvela.assignment40.prog2;
+
+public class MeetingRoom {
+	String room1="Nalanda";
+	String room2="Takshashila";
+	String room3="AgraFort";
+	String room4="PratapGadh";
+	int hr=1;
+	
+	void bookMeetingRoom() {
+		System.out.println("Meeting room "+room1+" is booked for "+hr+" hr");
+	}
+	void bookOtherMeetingRoom(String room,int hr) {
+		if(room.equals(room1))
+			System.out.println("This meeting room is already booked");
+		else
+		System.out.println("Meeting room "+room+ " is booked for "+hr+" hr");
+	}
+	void displayInfo() {
+		bookMeetingRoom();
+		bookOtherMeetingRoom("Takshashila",5);
+	}
+	public static void main(String[] args) {
+		MeetingRoom mr= new MeetingRoom();
+		mr.displayInfo();
+
+	}
+
+}
