1337f7d347917e527f232c1c707034f75a068149
Nagendra Tiwari <tiwarinagendra11@gmail.com>
2020-12-29 02:32:07 +0530

    NagendraT_Assignment_45

---
+++ src/nagendra/collectionPrograms/WordWithFrequency.java
@@ -0,0 +1,41 @@
+package nagendra.collectionPrograms;
+
+import java.util.LinkedHashMap;
+import java.util.Map.Entry;
+import java.util.Set;
+
+/*
+ Assignment - 45 : 27th Dec'2020 
+Print all the words having frequency less than or equal to 2. 
+String input = "Hi globant Hello Hi Hi globant Hi Hello Hello globant Hi Techno Credits Techno";
+output :  Techno -> 2
+                  Credits -> 1 
+*/
+
+public class WordWithFrequency {
+
+	void getDisplayFrequency(String input) {
+		String[] word = input.split(" ");
+		LinkedHashMap<String, Integer> map = new LinkedHashMap<String, Integer>();
+		for (int i = 0; i < word.length; i++) {
+			if (map.containsKey(word[i])) {
+				map.put(word[i], map.get(word[i]) + 1);
+			} else {
+				map.put(word[i], 1);
+			}
+		}
+		Set<Entry<String, Integer>> output = map.entrySet();
+
+		for (Entry<String, Integer> user : output) {
+			if (user.getValue() <= 2) {
+				System.out.println(user.getKey() + " ->" + user.getValue());
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		WordWithFrequency frequency = new WordWithFrequency();
+		String input = "Hi globant Hello Hi Hi globant Hi Hello Hello globant Hi Techno Credits Techno";
+		frequency.getDisplayFrequency(input);
+	}
+}
