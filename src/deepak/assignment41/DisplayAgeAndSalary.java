8198dcb14079df8fa81dfafcde09be941c3f8d52
Deepak Latad <dklatad@gmail.com>
2021-01-03 17:25:05 +0530

    Asgmt_41

---
+++ src/deepak/assignment41/DisplayAgeAndSalary.java
@@ -0,0 +1,52 @@
+package deepak.assignment41;
+
+import java.util.ArrayList;
+
+public class DisplayAgeAndSalary {
+	String firstName, middleName, lastName, empDeptName;
+	int empId, empDeptId, age;
+	int Salary;
+
+	DisplayAgeAndSalary(String firstName, String middleName, String lastName, String empDeptName, int empId,
+			int empDeptId, int age, int Salary) {
+		this.firstName = firstName;
+		this.middleName = middleName;
+		this.lastName = lastName;
+		this.empDeptId = empDeptId;
+		this.empId = empId;
+		this.age = age;
+		this.Salary = Salary;
+		this.empDeptName = empDeptName;
+	}
+
+	public String toString() {
+		System.out.println("Employee Details");
+		return "Employee name is :" + firstName + " " + lastName + "\nDepartment name is:" + empDeptName
+				+ "\nEmployee Age is:" + age + "\nEmpId is:" + empId + "\nDeptId is:" + empDeptId
+				+ "\nEmployee Salary is:" + Salary;
+	}
+
+	public static ArrayList<DisplayAgeAndSalary> findSalaryAndAge(ArrayList<DisplayAgeAndSalary> arrayList) {
+		ArrayList<DisplayAgeAndSalary> list = new ArrayList<DisplayAgeAndSalary>();
+		for (int i = 0; i < arrayList.size(); i++) {
+			if (arrayList.get(i).age < 30 && arrayList.get(i).Salary > 75000) {
+				list.add(arrayList.get(i));
+			}
+		}
+		return list;
+	}
+
+	public static void main(String[] args) {
+		ArrayList<DisplayAgeAndSalary> empDetails = new ArrayList<DisplayAgeAndSalary>();
+		DisplayAgeAndSalary emp1 = new DisplayAgeAndSalary("Amit", "Suresh", "Aphale", "Testing", 1026, 101, 30,
+				25000);
+		DisplayAgeAndSalary emp2 = new DisplayAgeAndSalary("Deepak", "Kisan", "Latad", "Testing", 1026, 102, 28,
+				76000);
+		DisplayAgeAndSalary emp3 = new DisplayAgeAndSalary("Aditya", "Harish", "Thakare", "Testing", 1026, 103, 32,
+				32000);
+		empDetails.add(emp1);
+		empDetails.add(emp2);
+		empDetails.add(emp3);
+		System.out.println(DisplayAgeAndSalary.findSalaryAndAge(empDetails));
+	}
+}
---
+++ src/deepak/assignment41/displayMinimumSalary.java
@@ -0,0 +1,56 @@
+package deepak.assignment41;
+
+import java.util.ArrayList;
+
+public class displayMinimumSalary {
+	String firstName, middleName, lastName, empDeptName;
+	int empId, empDeptId, age;
+	int Salary;
+
+	displayMinimumSalary(String firstName, String middleName, String lastName, String empDeptName, int empId,
+			int empDeptId, int age, int Salary) {
+		this.firstName = firstName;
+		this.middleName = middleName;
+		this.lastName = lastName;
+		this.empDeptId = empDeptId;
+		this.empId = empId;
+		this.age = age;
+		this.Salary = Salary;
+		this.empDeptName = empDeptName;
+	}
+
+	public String toString() {
+		System.out.println("Employee Details");
+		return "Employee name is :" + firstName + " " + lastName + "\nDepartment name is:" + empDeptName
+				+ "\nEmployee Age is:" + age + "\nEmpId is:" + empId + "\nDeptId is:" + empDeptId
+				+ "\nEmployee Salary is:" + Salary;
+	}
+
+	static displayMinimumSalary findMinSalary(ArrayList<displayMinimumSalary> empDetails) {
+		int minimumSalary = empDetails.get(0).Salary;
+		int minIndex = 0;
+		for (int i = 0; i < empDetails.size(); i++) {
+			if (empDetails.get(i).Salary < minimumSalary) {
+				minimumSalary = empDetails.get(i).Salary;
+				minIndex = i;
+			}
+		}
+		return empDetails.get(minIndex);
+	}
+
+	public static void main(String[] args) {
+		ArrayList<displayMinimumSalary> empDetails = new ArrayList<displayMinimumSalary>();
+		displayMinimumSalary emp1 = new displayMinimumSalary("Amit", "Suresh", "Aphale", "Testing", 1026, 101, 25,
+				25000);
+		displayMinimumSalary emp2 = new displayMinimumSalary("Deepak", "Kisan", "Latad", "Testing", 1026, 102, 26,
+				30000);
+		displayMinimumSalary emp3 = new displayMinimumSalary("Aditya", "Harish", "Thakare", "Testing", 1026, 103, 24,
+				22000);
+		empDetails.add(emp1);
+		empDetails.add(emp2);
+		empDetails.add(emp3);
+
+		System.out.println(displayMinimumSalary.findMinSalary(empDetails));
+	}
+
+}
