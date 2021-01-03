038f2fc5b51e1c89939602da78404efa35d0d6e6
Suvela khaladkar <suvela.vela16@gmail.com>
2021-01-03 08:33:13 +0530

    Suvela_assignment41

--- src/suvela/assignment41/ArrayList1.java
+++ src/suvela/assignment41/ArrayList1.java
@@ -6,10 +6,9 @@ import suvela.assignment41.Employee;
 public class ArrayList1 {
 	public Employee printInfo(ArrayList<Employee> empList) {
 		int minSalaryIndex = 0;
-		int min=0;
 		for (int index = 0; index < empList.size(); index++) {
 			Employee e = empList.get(index);
-			if (e.getSalary() > min) {
+			if (e.getSalary() < empList.get(minSalaryIndex).Salary) {
 				minSalaryIndex = index;
 			}
 		}
@@ -18,9 +17,9 @@ public class ArrayList1 {
 
 	public static void main(String[] args) {
 		ArrayList<Employee> empList = new ArrayList<>();
-		Employee e1 = new Employee("Suvela", "Khaladkar", "ABC", 24, 1090, 1111, 50000);
-		Employee e2 = new Employee("Ankita", "Dahiwelkar", "ABC", 26, 1091, 11188, 40000);
-		Employee e3 = new Employee("Aishwarya", "Sathe", "PQR", 25, 1067, 1541, 30000);
+		Employee e1 = new Employee("Suvela", "Khaladkar", "ABC", 24, 1090, 1111, 40000);
+		Employee e2 = new Employee("Ankita", "Dahiwelkar", "ABC", 26, 1091, 11188, 50000);
+		Employee e3 = new Employee("Aishwarya", "Sathe", "PQR", 25, 1067, 1541, 60000);
 
 		empList.add(e1);
 		empList.add(e2);
@@ -31,3 +30,4 @@ public class ArrayList1 {
 	}
 
 }
+
