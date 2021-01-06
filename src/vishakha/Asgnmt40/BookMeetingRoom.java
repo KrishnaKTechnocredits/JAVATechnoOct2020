8ea6afcbc85705c3d465ceb3e341c5f77577a8c9
Vishakha Borse <vishakhaborse77@gmail.com>
2021-01-07 00:21:37 +0530

    VishakhaB_Asgnmt_40

---
+++ src/vishakha/Asgnmt40/BookMeetingRoom.java
@@ -0,0 +1,48 @@
+
+/**
+Create a class MeetingRoom having method bookMeetingRoom which should provide
+below facility.
+meeting room : Nalanda, Takshashila, AgraFort, PratapGadh
+a. “Nalanda” meeting room will be booked with default bookMeetingRoom() method
+for 1 hr.
+b. Write another method which helps to book Other then “Nalanda” meeting room by
+giving name of meeting room and hrs as parameter.
+c. displayInfo method should print booked meeting room info [i.e. meeting room,
+booked for how many hrs]
+*/
+
+package vishakha.Asgnmt40;
+
+public class BookMeetingRoom {
+
+	String[] meetingRooms = {"Nalanda", "Takshashila", "AgraFort", "PratapGadh"};
+	boolean[] isRoomBooked= {false,false,false,false};
+	int[] hoursBooked= {0,0,0,0};
+	void bookMeetingRoom() {
+		bookMeetingRoom("Nalanda",1);
+	}
+
+	void bookMeetingRoom(String roomName, int hours) {
+		for(int index=0; index<meetingRooms.length;index++) {
+			if(meetingRooms[index].equalsIgnoreCase(roomName)) {
+				isRoomBooked[index]=true;
+				hoursBooked[index]=hours;
+			}
+		}
+	}
+	
+	void displayInfo(){
+		for(int index=0; index<isRoomBooked.length;index++) {
+			if(isRoomBooked[index])
+				System.out.println("Meeting room booked, NameOfRoom: "+meetingRooms[index]+", HoursBooked: "+hoursBooked[index]);
+		}
+	}
+	
+	public static void main(String[] args) {
+		BookMeetingRoom meetingRoom = new BookMeetingRoom();
+		meetingRoom.bookMeetingRoom();
+		meetingRoom.bookMeetingRoom("Takshshila", 3);
+		meetingRoom.bookMeetingRoom("PratapGadh", 2);
+		meetingRoom.displayInfo();
+	}
+}
