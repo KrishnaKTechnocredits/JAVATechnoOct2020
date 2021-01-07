9f3cb4aaa2bcce9e0fec7fd373c6280a1c3b2656
Surbhi Jain <jain.surbhi1605@gmail.com>
2021-01-07 11:39:43 +0530

    src/surbhi/Assignment27.java

---
+++ src/surbhi/Assignment27.java
@@ -0,0 +1,34 @@
+package surbhi;
+
+/*
+	print all the words which does't have any digit. 
+	String[] input = {"Masgj3Pw", "r4fsjk","techno","sfjk44lk"};
+	output : techno
+	*/
+
+import java.util.Arrays;
+
+public class Assignment27 {
+
+	void printWordsWithoutDigit(String[] input) {
+		System.out.println("Given String Array: " + Arrays.toString(input));
+		System.out.println("Words which doesn't have any digit: ");
+		for (int index = 0; index < input.length; index++) {
+			if (!isWordContainsDigit(input[index]))
+				System.out.println(input[index]);
+		}
+	}
+
+	boolean isWordContainsDigit(String string) {
+		for (int index = 0; index < string.length(); index++) {
+			if (Character.isDigit(string.charAt(index)))
+				return true;
+		}
+		return false;
+	}
+
+	public static void main(String[] args) {
+		String[] input = { "Masgj3Pw", "r4fsjk", "techno", "sfjk44lk", "Masala@ty", "value&56" };
+		new Assignment27().printWordsWithoutDigit(input);
+	}
+}
