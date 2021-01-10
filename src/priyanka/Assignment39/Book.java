83b1d9af73ccfeb05251be8b2f1bffd97303ea45
Priyanka kamthe <priya8542@gmail.com>
2021-01-10 19:38:00 +0530

    Priyanka_Assignment_39

---
+++ src/priyanka/Assignment39/Book.java
@@ -0,0 +1,24 @@
+package priyanka.Assignment39;
+
+public class Book {
+	String title;
+	String bookPublisher;
+	String author;
+	public Book(String title, String bookPublisher, String author) {
+		this.title=title;
+		this.bookPublisher=bookPublisher;
+		this.author=author;
+	}
+	
+	public void displayBookDetails() {
+		System.out.println("Book Title: "+title+", Book Publisher: "+bookPublisher+", Book Author: "+author);
+	}
+
+	public static void main(String[] args) {
+		Book book1 = new Book("The Other Side of the Sky", "Amazon", "Amie Kaufman");
+		Book book2 = new Book("2 States", "Amazon", "Chetan Bhagat");
+		book1.displayBookDetails();
+		book2.displayBookDetails();
+	}
+
+}
---
+++ src/priyanka/Assignment39/TestSquareClass.java
@@ -0,0 +1,10 @@
+package priyanka.Assignment39;
+
+public class TestSquareClass {
+	
+	public static void main(String[] args) {
+		Square square = new Square(10);
+		System.out.println("Area of side 10: "+square.areaOfSquare());
+	}
+
+}
