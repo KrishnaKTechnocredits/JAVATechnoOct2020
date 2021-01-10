d826ce4c6985706fc8585e037f2d0903237568a9
Priyanka kamthe <priya8542@gmail.com>
2021-01-10 21:58:19 +0530

    Priyanka_Assignment_48

---
+++ src/priyanka/Assignment48/Student.java
@@ -0,0 +1,23 @@
+package priyanka.Assignment48;
+
+public class Student implements Comparable<Student> {
+	String studentName;
+	int studentId;
+	int studentStd;
+
+	public Student(String studentName, int studentId, int studentStd) {
+		this.studentName = studentName;
+		this.studentId = studentId;
+		this.studentStd = studentStd;
+	}
+
+	@Override
+	public int compareTo(Student object) {
+		return this.studentName.compareTo(object.studentName);
+	}
+
+	@Override
+	public String toString() {
+		return "Student Name: " + this.studentName + ", Student Id: " + studentId + ", Student Std: " + studentStd;
+	}
+}
