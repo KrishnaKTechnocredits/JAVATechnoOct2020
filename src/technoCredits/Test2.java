6cf7daa779d73bc244b5b1b7a2eaaba6e5f3a5da
Krishna Kanani <kananikrishna427@gmail.com>
2021-03-08 21:02:24 +0530

    Added changes

--- src/technoCredits/Test2.java
+++ src/technoCredits/Test2.java
@@ -2,7 +2,8 @@ package technoCredits;
 public class Test2 {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to the TechnoCredits !!!");
+		System.out.println("Welcome to the TechnoCredits 123456 !!!");
+		System.out.println("In A");
 	}
 
 }
