581e26b221efd7510865970e41e2790184c9d0bf
Krishna Kanani <kananikrishna427@gmail.com>
2021-03-09 20:32:45 +0530

    T2

--- src/technoCredits/Test2.java
+++ src/technoCredits/Test2.java
@@ -4,6 +4,7 @@ public class Test2 {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the TechnoCredits !!!");
 		System.out.println("In T2");
+		System.out.println("In T22222");
 	}
 
 }
