4f65070734147354208993b46d21db8387758234
Krishna Kanani <kananikrishna427@gmail.com>
2021-03-09 18:56:41 +0530

    Added 678

--- src/technoCredits/Test2.java
+++ src/technoCredits/Test2.java
@@ -4,6 +4,10 @@ public class Test2 {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the TechnoCredits 123456 !!!");
 		System.out.println("In A");
+		
+		
+		System.out.println("In B1");
+		System.out.println("In B");
 	}
 
 }
