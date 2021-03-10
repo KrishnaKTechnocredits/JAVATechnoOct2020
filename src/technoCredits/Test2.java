83b632f6962bedab11305c37f8a3c69449f43e43
Krishna Kanani <kananikrishna427@gmail.com>
2021-03-10 08:22:16 +0530

    A

--- src/technoCredits/Test2.java
+++ src/technoCredits/Test2.java
@@ -4,8 +4,8 @@ public class Test2 {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the TechnoCredits !!!");
 		System.out.println("In AA");
-		System.out.println("In AA 11");
-		System.out.println("In AA 12");
+		System.out.println("In AA 10");
+		System.out.println("In AA 10");
 	}
 
 }
