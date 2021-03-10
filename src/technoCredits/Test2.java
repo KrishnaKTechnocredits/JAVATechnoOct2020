68478efeaaf474c2237233412b8a836221bf8485
Krishna Kanani <kananikrishna427@gmail.com>
2021-03-10 08:17:58 +0530

    Added changes

--- src/technoCredits/Test2.java
+++ src/technoCredits/Test2.java
@@ -3,6 +3,8 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to TechnoCredits 01234567 !!!");
+		System.out.println("T2");
+		System.out.println("T2 22222");
 	}
 
 }
