ca4cb6dd51e0e24af5694ff06355ad43c782f8bc
Krishna Kanani <kananikrishna427@gmail.com>
2021-03-10 08:27:41 +0530

    Resolveconflicts

--- src/technoCredits/Test2.java
+++ src/technoCredits/Test2.java
@@ -3,9 +3,9 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to the TechnoCredits !!!");
-		System.out.println("In AA");
-		System.out.println("In AA 10");
-		System.out.println("In AA 10");
+		System.out.println("In BB");
+		System.out.println("In BB 11");
+		System.out.println("In BB 12");
 	}
 
 }
